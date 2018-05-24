24c1121347a97758cbebee0401b2f9d068aafa3e
Brennan Cheung <git@brennancheung.com>
2018-05-24 12:46:13 -0700

    Add more GraphQL functionality for tenants

--- src/app/plugins/openstack/components/tenants/AddTenantPage.js
+++ src/app/plugins/openstack/components/tenants/AddTenantPage.js
@@ -1,18 +1,22 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { addTenant } from '../../actions/tenants'
 import AddTenantForm from './AddTenantForm'
+import { ADD_TENANT } from './actions'
+import { compose, withApollo } from 'react-apollo'
+import requiresAuthentication from '../../util/requiresAuthentication'
 
-const mapStateToProps = state => ({})
-
-@withRouter
-@connect(mapStateToProps)
 class AddTenantPage extends React.Component {
   handleSubmit = tenant => {
-    const { dispatch, history } = this.props
+    const { client, history } = this.props
     try {
-      dispatch(addTenant(tenant))
+      console.log('client.mutate')
+      console.log(tenant)
+      client.mutate({
+        mutation: ADD_TENANT,
+        variables: {
+          input: tenant
+        }
+      })
       history.push('/ui/openstack/tenants')
     } catch (err) {
       console.error(err)
@@ -29,4 +33,8 @@ class AddTenantPage extends React.Component {
   }
 }
 
-export default AddTenantPage
+export default compose(
+  requiresAuthentication,
+  withRouter,
+  withApollo,
+)(AddTenantPage)
