472634b471847b4bcb0299b933eaadc6ba3a189e
David.L <liuhf24@gmail.com>
2018-06-06 18:53:43 -0700

    Edited style details.

--- src/stories/UI/loginPage.js
+++ src/stories/UI/loginPage.js
@@ -4,32 +4,24 @@ import { Paper, Typography, Grid, TextField, Button } from '@material-ui/core'
 
 const styles = theme => ({
   root: {
-    // display: 'flex',
-    position: 'absolute',
-    top: '10%',
+    marginTop: '5%',
+    paddingTop: 25,
     paddingBottom: 50,
     overflow: 'auto'
   },
   paper: {
-    // width: 400,
-    // paddingLeft: 24,
-    // paddingRight: 24,
-    paddingBottom: 20
+    paddingBottom: 35
   },
   img: {
     maxHeight: '50%',
     maxWidth: '50%',
     display: 'block',
-    paddingTop: 24,
-    paddingBottom: 25,
+    paddingTop: 25,
     marginLeft: 'auto',
     marginRight: 'auto'
   },
-  formControl: {
-    minWidth: '100%',
-    visibility: 'display'
-  },
   form: {
+    paddingTop: 25,
     marginLeft: '10%',
     marginRight: '10%'
   },
@@ -39,7 +31,7 @@ const styles = theme => ({
   },
   signinButton: {
     minWidth: '80%',
-    marginTop: 20,
+    marginTop: 35,
     display: 'block',
     marginLeft: 'auto',
     marginRight: 'auto'
@@ -47,44 +39,12 @@ const styles = theme => ({
 })
 
 class LoginPage extends React.Component {
-  state = {
-    username: '',
-    password: '',
-    MFAcheckbox: false,
-    method: '',
-    open: false,
-    SSOhidden: false
-  }
-
-  renderHeader = () => (
-    <div style={{ backgroundColor: '#4aa3df', width: '200px', padding: '0 2rem' }}>
-      <img src="/logo.png" style={{ width: '200px' }} />
-    </div>
-  )
-
-  renderFooter = () => (
-    // TODO
-    null
-  )
-
   render () {
-    // const { username, password } = this.state
     const { classes } = this.props
     return (
-      <div>
-        <Grid
-          container
-          className={classes.root}
-          justify="center"
-          alignItems="center"
-          direction="row"
-        >
-          <Grid
-            item
-            md={4}
-            sm={6}
-            xs={12}
-          >
+      <div className={classes.root}>
+        <Grid container justify="center">
+          <Grid item md={4} lg={3}>
             <Paper className={classes.paper}>
               <img src="https://hostadvice.com/wp-content/uploads/2017/07/Platform9-LogoStacked-777x352.png" className={classes.img} />
               <form className={classes.form}>
@@ -92,31 +52,10 @@ class LoginPage extends React.Component {
                   Please sign in
                 </Typography>
                 <Fragment>
-                  <TextField
-                    required
-                    id="email"
-                    label="Email"
-                    className={classes.textField}
-                    placeholder="Email"
-                    margin="normal"
-                  />
-                  <br />
-                  <TextField
-                    required
-                    id="password"
-                    label="Password"
-                    className={classes.textField}
-                    type="password"
-                    margin="normal"
-                  />
+                  <TextField required id="email" label="Email" placeholder="Email" className={classes.textField} />
+                  <TextField required id="password" label="Password" className={classes.textField} type="password" />
                 </Fragment>
-                <br />
-                <Button
-                  className={classes.signinButton}
-                  variant="contained"
-                  color="primary"
-                  size="large"
-                >
+                <Button className={classes.signinButton} variant="contained" color="primary">
                   SIGN IN
                 </Button>
               </form>
@@ -124,16 +63,6 @@ class LoginPage extends React.Component {
           </Grid>
         </Grid>
       </div>
-
-      // <div className="login-page">
-      //   {this.renderHeader()}
-      //   {this.renderStatus()}
-      //   <h2>Please sign in</h2>
-      //   <div><input className="login-username" type="text" value={username} onChange={this.updateValue('username')} /></div>
-      //   <div><input className="login-password" type="password" value={password} onChange={this.updateValue('password')} /></div>
-      //   <div><Button className="login-submit" color="primary" variant="raised" onClick={this.performLogin}>Log in</Button></div>
-      //   {this.renderFooter()}
-      // </div>
     )
   }
 }
