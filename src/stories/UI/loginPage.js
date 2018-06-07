9aa8c79d129d528f935828c3f08d6b507feface0
David.L <liuhf24@gmail.com>
2018-06-06 20:00:37 -0700

    Add checkbox and footer.

--- src/stories/UI/loginPage.js
+++ src/stories/UI/loginPage.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import { Paper, Typography, Grid, TextField, Button } from '@material-ui/core'
+import { Paper, Typography, Grid, TextField, Button, FormControlLabel, Checkbox } from '@material-ui/core'
 
 const styles = theme => ({
   root: {
     marginTop: '5%',
     paddingTop: 25,
-    paddingBottom: 50,
+    paddingBottom: 30,
     overflow: 'auto'
   },
   paper: {
-    paddingBottom: 35
+    paddingBottom: 25
   },
   img: {
     maxHeight: '50%',
@@ -29,16 +29,34 @@ const styles = theme => ({
     minWidth: '100%',
     marginTop: 10
   },
+  checkbox: {
+    marginTop: 20
+  },
   signinButton: {
     minWidth: '80%',
-    marginTop: 35,
+    marginTop: 25,
     display: 'block',
     marginLeft: 'auto',
     marginRight: 'auto'
+  },
+  forgotPwd: {
+    marginTop: 15,
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  paragraph: {
+    marginBottom: 10,
+    textAlign: 'center'
   }
 })
 
 class LoginPage extends React.Component {
+  state = {
+    MFAcheckbox: false
+  }
+  handleChangeBox = name => event => {
+    this.setState({ [name]: event.target.checked })
+  }
   render () {
     const { classes } = this.props
     return (
@@ -54,11 +72,47 @@ class LoginPage extends React.Component {
                 <Fragment>
                   <TextField required id="email" label="Email" placeholder="Email" className={classes.textField} />
                   <TextField required id="password" label="Password" className={classes.textField} type="password" />
+                  <FormControlLabel
+                    className={classes.checkbox}
+                    control={
+                      <Checkbox
+                        checked={this.state.MFAcheckbox}
+                        onChange={this.handleChangeBox('MFAcheckbox')}
+                        color="primary"
+                      />
+                    }
+                    label={
+                      <div>
+                        <span>I have a Multi-Factor Authentication (MFA) token.</span>
+                        <a href="http://www.platform9.com"> (more info)</a>
+                      </div>
+                    }
+                  />
+                  { !this.state.MFAcheckbox
+                    ? null
+                    : <TextField
+                      required={this.state.MFAcheckbox}
+                      id="MFA"
+                      label="MFA Code"
+                      className={classes.textField}
+                      placeholder="MFA Code"
+                      margin="normal"
+                    />
+                  }
                 </Fragment>
                 <Button className={classes.signinButton} variant="contained" color="primary">
                   SIGN IN
                 </Button>
+                <Typography className={classes.forgotPwd} gutterBottom>
+                  <a href="http://www.platform9.com">Forgot password?</a>
+                </Typography>
               </form>
+              <Typography className={classes.paragraph} variant="caption" color="textSecondary">
+                By signing in, you agree to our <a href="http://www.platform9.com">Terms of Service</a>.
+              </Typography>
+              <Typography className={classes.paragraph} variant="caption" color="textSecondary">
+                © 2014-2018 Platform9 Systems, Inc.
+              </Typography>
             </Paper>
           </Grid>
         </Grid>
