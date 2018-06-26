fd0a4c18e2c3ddedbb13c4e5cee7dd736ae07193
David.L <liuhf24@gmail.com>
2018-06-26 14:06:04 -0700

    Rename Network to Neutron.

--- src/openstack-client/__tests__/Neutron.test.js
+++ src/openstack-client/__tests__/Neutron.test.js
@@ -2,102 +2,102 @@ import {
   makeRegionedClient
 } from '../helpers'
 
-describe('Networks', () => {
+describe('Neutron', () => {
   jest.setTimeout(30000)
 
   it('set region urls', async () => {
     const client = await makeRegionedClient()
-    const urls = await client.network.setRegionUrls()
+    const urls = await client.neutron.setRegionUrls()
     expect(urls).toBeDefined()
   })
 
   it('list networks', async () => {
     const client = await makeRegionedClient()
-    const networks = await client.network.getNetworks()
+    const networks = await client.neutron.getNetworks()
     expect(networks).toBeDefined()
   })
 
   it('list region networks', async () => {
     const client = await makeRegionedClient()
-    const networks = await client.network.getNetworksForRegion(client.activeRegion)
+    const networks = await client.neutron.getNetworksForRegion(client.activeRegion)
     expect(networks).toBeDefined()
   })
 
   it('create, get and delete a network placeholder', async () => {
     const client = await makeRegionedClient()
-    const network = await client.network.createNetwork({})
+    const network = await client.neutron.createNetwork({})
     expect(network.id).toBeDefined()
 
-    const newNetwork = await client.network.getNetwork(network.id)
+    const newNetwork = await client.neutron.getNetwork(network.id)
     expect(newNetwork).toBeDefined()
 
-    await client.network.deleteNetwork(newNetwork.id)
+    await client.neutron.deleteNetwork(newNetwork.id)
 
-    const newNetworks = await client.network.getNetworks()
+    const newNetworks = await client.neutron.getNetworks()
     expect(newNetworks.find(x => x.id === newNetwork.id)).not.toBeDefined()
   })
 
   it('create, update and delete a network placeholder', async () => {
     const client = await makeRegionedClient()
-    const network = await client.network.createNetwork({})
+    const network = await client.neutron.createNetwork({})
     expect(network.id).toBeDefined()
 
-    const updatedNetwork = await client.network.updateNetwork(network.id, {
+    const updatedNetwork = await client.neutron.updateNetwork(network.id, {
       name: 'New Name'
     })
     expect(updatedNetwork.name).toBe('New Name')
 
-    await client.network.deleteNetwork(updatedNetwork.id)
+    await client.neutron.deleteNetwork(updatedNetwork.id)
 
-    const newNetworks = await client.network.getNetworks()
+    const newNetworks = await client.neutron.getNetworks()
     expect(newNetworks.find(x => x.id === updatedNetwork.id)).not.toBeDefined()
   })
 
   it('get subnets', async () => {
     const client = await makeRegionedClient()
-    const subnets = await client.network.getSubnets()
+    const subnets = await client.neutron.getSubnets()
     expect(subnets).toBeDefined()
   })
 
   it('create, update and delete a subnet placeholder', async () => {
     const client = await makeRegionedClient()
-    const network = await client.network.createNetwork({
+    const network = await client.neutron.createNetwork({
       name: 'To Test Subnet'
     })
-    const subnet = await client.network.createSubnet({
+    const subnet = await client.neutron.createSubnet({
       name: 'Test Subnet',
       ip_version: 4,
       cidr: '10.0.3.0/24',
       network_id: network.id
     })
-    const updatedSubnet = await client.network.updateSubnet(subnet.id, {
+    const updatedSubnet = await client.neutron.updateSubnet(subnet.id, {
       name: 'Updated Test Subnet'
     })
     expect(updatedSubnet.name).toBe('Updated Test Subnet')
-    await client.network.deleteSubnet(updatedSubnet.id)
-    await client.network.deleteNetwork(network.id)
+    await client.neutron.deleteSubnet(updatedSubnet.id)
+    await client.neutron.deleteNetwork(network.id)
   })
 
   it('get ports', async () => {
     const client = await makeRegionedClient()
-    const ports = await client.network.getPorts()
+    const ports = await client.neutron.getPorts()
     expect(ports).toBeDefined()
   })
 
   it('create, update and delete a port placeholder', async () => {
     const client = await makeRegionedClient()
-    const network = await client.network.createNetwork({
+    const network = await client.neutron.createNetwork({
       name: 'To Test Port'
     })
-    const port = await client.network.createPort({
+    const port = await client.neutron.createPort({
       name: 'Test Port',
       network_id: network.id
     })
-    const updatedPort = await client.network.updatePort(port.id, {
+    const updatedPort = await client.neutron.updatePort(port.id, {
       name: 'Updated Test Port'
     })
     expect(updatedPort.name).toBe('Updated Test Port')
-    await client.network.deletePort(updatedPort.id)
-    await client.network.deleteNetwork(network.id)
+    await client.neutron.deletePort(updatedPort.id)
+    await client.neutron.deleteNetwork(network.id)
   })
 })
