5c575e67477a927f67a329cdb3f2de7fd850afa5
Brennan Cheung <git@brennancheung.com>
2018-10-08 14:28:43 -0700

    Qbert API

--- src/openstack-client/OpenstackClient.js
+++ src/openstack-client/OpenstackClient.js
@@ -1,9 +1,12 @@
+import axios from 'axios'
+
 import Cinder from './Cinder'
 import Glance from './Glance'
 import Keystone from './Keystone'
+import Murano from './Murano'
 import Neutron from './Neutron'
 import Nova from './Nova'
-import Murano from './Murano'
+import Qbert from './Qbert'
 import ResMgr from './ResMgr'
 
 class OpenstackClient {
@@ -19,6 +22,7 @@ class OpenstackClient {
     this.nova = new Nova(this)
     this.murano = new Murano(this)
     this.resmgr = new ResMgr(this)
+    this.qbert = new Qbert(this)
 
     this.catalog = {}
     this.activeRegion = null
@@ -30,6 +34,7 @@ class OpenstackClient {
       unscopedToken: this.unscopedToken,
       scopedToken: this.scopedToken,
       catalog: this.catalog,
+      activeProjectId: this.activeProjectId,
     }
   }
 
@@ -56,6 +61,42 @@ class OpenstackClient {
         'X-Auth-Token': token
       }
     })
+  }
+
+  async basicGet (url) {
+    try {
+      const response = await axios.get(url, this.getAuthHeaders())
+      return response.data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async basicPost (url, body) {
+    try {
+      const response = await axios.post(url, body, this.getAuthHeaders())
+      return response.data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async basicPut (url, body) {
+    try {
+      const response = await axios.put(url, body, this.getAuthHeaders())
+      return response.data
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async basicDelete (url) {
+    try {
+      const response = await axios.delete(url, this.getAuthHeaders())
+      return response.data
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
 
