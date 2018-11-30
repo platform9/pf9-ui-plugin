c34716c804fbd734d3d5259373cee6f162d60fbd
Brennan Cheung <git@brennancheung.com>
2018-11-29 19:00:24 -0800

    Cluster details page WIP

---
+++ src/app/plugins/kubernetes/components/infrastructure/ClusterDetailsPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Tabs from 'core/common/Tabs'
+import Tab from 'core/common/Tab'
+
+// import ClusterInfo from './ClusterInfo'
+// import ClusterNodes from './ClusterNodes'
+const ClusterInfo = () => <h1>Info</h1>
+const ClusterNodes = () => <h1>Nodes</h1>
+
+const ClusterDetailsPage = () => (
+  <Tabs>
+    <Tab value="info" label="Info"><ClusterInfo /></Tab>
+    <Tab value="nodes" label="Nodes"><ClusterNodes /></Tab>
+  </Tabs>
+)
+
+export default ClusterDetailsPage
