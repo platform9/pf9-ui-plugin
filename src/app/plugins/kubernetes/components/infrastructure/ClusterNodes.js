4acd1547655c17bc1bccf3e28d4cbe1d0e8844dd
Brennan Cheung <git@brennancheung.com>
2018-12-05 14:57:08 -0800

    Refactor nodes and cluster nodes to use the same code

--- src/app/plugins/kubernetes/components/infrastructure/ClusterNodes.js
+++ src/app/plugins/kubernetes/components/infrastructure/ClusterNodes.js
@@ -4,51 +4,23 @@ import { withAppContext } from 'core/AppContext'
 import { compose } from 'ramda'
 import { loadInfrastructure } from './actions'
 import { withDataLoader } from 'core/DataLoader'
-import { utilizationRenderer } from './NodesListPage'
 import ListTable from 'core/common/list_table/ListTable'
-import HostStatus from 'core/common/HostStatus'
-import { localizeRole } from 'api-client/ResMgr'
 
-const renderCluster = (name, node) => name + (node.isMaster ? ' (master)' : '')
-
-const statusRenderer = (_, node) => {
-  const fullNode = context.nodes.find(x => x.uuid === node.uuid)
-  return <HostStatus host={fullNode.combined} />
-}
-
-const statsRenderer = stat => (_, node, context) => {
-  const fullNode = context.nodes.find(x => x.uuid === node.uuid)
-  return utilizationRenderer(stat)(_, fullNode)
-}
-
-const renderRoles = (_, node, context) => {
-  const fullNode = context.nodes.find(x => x.uuid === node.uuid)
-  return fullNode.combined.roles.map(localizeRole).join(', ')
-}
-
-const columns = [
-  { id: 'uuid', label: 'UUID', display: false },
-  { id: 'name', label: 'Name' },
-  { id: 'status', label: 'Status', render: statusRenderer },
-  { id: 'primaryIp', label: 'Primary IP' },
-  { id: 'clusterName', label: 'Cluster', render: renderCluster },
-  { id: 'compute', label: 'Compute', render: statsRenderer('compute') },
-  { id: 'memory', label: 'Memory', render: statsRenderer('memory') },
-  { id: 'storage', label: 'Storage', render: statsRenderer('disk') },
-  { id: 'roles', label: 'Assigned Roles', render: renderRoles },
-]
+// This table essentially has the same functionality as the <NodesList>
+// except that it is only the nodes from the a single cluster.
+import { columns } from './NodesListPage'
 
 const ClusterNodes = ({ context, data, match }) => {
   const cluster = data.find(x => x.uuid === match.params.id)
+  const nodes = cluster.nodes.map(node => context.nodes.find(x => x.uuid === node.uuid))
   return (
     <div>
       <ListTable
         title="Cluster Nodes"
         columns={columns}
-        data={cluster.nodes}
+        data={nodes}
         uniqueIdentifier="uuid"
       />
-      <pre>{JSON.stringify(cluster.nodes, null, 4)}</pre>
     </div>
   )
 }
--- src/app/plugins/kubernetes/components/infrastructure/NodesListPage.js
+++ src/app/plugins/kubernetes/components/infrastructure/NodesListPage.js
@@ -1,31 +1,48 @@
 import React from 'react'
 import createCRUDComponents from 'core/createCRUDComponents'
 import { loadInfrastructure } from './actions'
-import { pathOrNull } from 'core/fp'
+import { maybeFnOrNull } from 'core/fp'
+import { pipe } from 'ramda'
 import HostStatus from 'core/common/HostStatus'
+import { localizeRole } from 'api-client/ResMgr'
+import { castFuzzyBool, columnPathLookup, castBoolToStr } from 'utils/misc'
 
-export const statusRenderer = (contents, node) => (<HostStatus host={node.combined} />)
+const renderCluster = (name, node) => name + (node.isMaster ? ' (master)' : '')
 
-export const utilizationRenderer = field => (contents, node) => {
-  const stats = pathOrNull(`combined.usage.${field}`)(node)
-  if (!stats) { return null }
-  // TODO: make this look nicer
-  return <span>{stats.current.toFixed(2)} / {stats.max.toFixed(2)}{stats.units} used</span>
-}
+const statusRenderer = (_, node) => (<HostStatus host={node.combined} />)
+
+const utilizationRenderer = field => pipe(
+  columnPathLookup(`combined.usage.${field}`),
+  // Offline nodes won't have usage stats
+  maybeFnOrNull(stat =>
+    <span>{stat.current.toFixed(2)} / {stat.max.toFixed(2)}{stat.units} used</span>
+  )
+)
+
+const renderRoles = (_, node) => node.combined.roles.map(localizeRole).join(', ')
+
+const getSpotInstance = pipe(
+  columnPathLookup('combined.resmgr.extensions.node_metadata.data.isSpotInstance'),
+  castFuzzyBool,
+  castBoolToStr(),
+)
+
+export const columns = [
+  { id: 'uuid', label: 'UUID', display: false },
+  { id: 'name', label: 'Name' },
+  { id: 'status', label: 'Status', render: statusRenderer },
+  { id: 'primaryIp', label: 'Primary IP' },
+  { id: 'compute', label: 'Compute', render: utilizationRenderer('compute') },
+  { id: 'memory', label: 'Memory', render: utilizationRenderer('memory') },
+  { id: 'storage', label: 'Storage', render: utilizationRenderer('disk') },
+  { id: 'clusterName', label: 'Cluster', renderCluster },
+  { id: 'isSpotInstance', label: 'Spot Instance?', render: getSpotInstance },
+  { id: 'assignedRoles', label: 'Assigned Roles', render: renderRoles },
+]
 
 export const options = {
   baseUrl: '/ui/kubernetes/infrastructure/nodes',
-  columns: [
-    { id: 'name', label: 'Name' },
-    { id: 'status', label: 'Status', render: statusRenderer },
-    { id: 'primaryIp', label: 'Primary IP' },
-    { id: 'compute', label: 'Compute', render: utilizationRenderer('compute') },
-    { id: 'memory', label: 'Memory', render: utilizationRenderer('memory') },
-    { id: 'storage', label: 'Storage', render: utilizationRenderer('disk') },
-    { id: 'clusterName', label: 'Cluster' },
-    { id: 'isSpotInstance', label: 'Spot Instance?' },
-    { id: 'assignedRoles', label: 'Assigned Roles' },
-  ],
+  columns,
   dataKey: 'nodes',
   loaderFn: loadInfrastructure,
   name: 'Nodes',
