6d8df4295de9778d640facb1ef50a6d1eb79f748
Brennan Cheung <git@brennancheung.com>
2018-12-05 17:56:38 -0800

    Display usage stats graphically for nodes

--- src/app/plugins/kubernetes/components/infrastructure/NodesListPage.js
+++ src/app/plugins/kubernetes/components/infrastructure/NodesListPage.js
@@ -7,6 +7,7 @@ import HostStatus from 'core/common/HostStatus'
 import SimpleLink from 'core/common/SimpleLink'
 import { localizeRoles } from 'api-client/ResMgr'
 import { castFuzzyBool, columnPathLookup, castBoolToStr } from 'utils/misc'
+import ProgressBar from 'core/common/ProgressBar'
 
 const renderStatus = (_, node) => (<HostStatus host={node.combined} />)
 const isMaster = pipe(castFuzzyBool, castBoolToStr())
@@ -16,12 +17,23 @@ const renderRoles = (_, node) => {
   return localizeRoles(roles).join(', ')
 }
 
+const UsageBar = ({ stat }) => {
+  const percent = Math.round((stat.current * 100) / stat.max)
+  const cur = stat.current.toFixed(2)
+  const max = stat.max.toFixed(2)
+  return (
+    <ProgressBar
+      compact
+      percent={percent}
+      label={p => <span><strong>{p}%</strong> - {cur} / {max}{stat.units} {stat.type}</span>}
+    />
+  )
+}
+
 const renderStats = field => pipe(
   columnPathLookup(`combined.usage.${field}`),
   // Offline nodes won't have usage stats
-  maybeFnOrNull(stat =>
-    <span>{stat.current.toFixed(2)} / {stat.max.toFixed(2)}{stat.units} used</span>
-  )
+  maybeFnOrNull(stat => <UsageBar stat={stat} />)
 )
 
 const renderLogs = url => <SimpleLink src={url} target="_blank">logs</SimpleLink>
