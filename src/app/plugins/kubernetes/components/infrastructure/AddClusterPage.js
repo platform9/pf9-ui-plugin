4a84a0f8ca0c4ce5658e920ec5b8b81abbeb3d0f
Brennan Cheung <git@brennancheung.com>
2019-01-02 17:08:08 -0800

    Fix cloudProviders in simulator

--- src/app/plugins/kubernetes/components/infrastructure/AddClusterPage.js
+++ src/app/plugins/kubernetes/components/infrastructure/AddClusterPage.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { withDataLoader } from 'core/DataLoader'
 import FormWrapper from 'core/common/FormWrapper'
 import PicklistField from 'core/common/validated_form/PicklistField'
 import ValidatedForm from 'core/common/validated_form/ValidatedForm'
@@ -7,16 +6,14 @@ import Wizard from 'core/common/Wizard'
 import WizardStep from 'core/common/WizardStep'
 import { compose } from 'ramda'
 import { loadCloudProviders } from './actions'
-import { withAppContext } from 'core/AppContext'
+import { withDataLoader } from 'core/DataLoader'
 
 class AddClusterPage extends React.Component {
+  handleSubmit = () => console.log('TODO: AddClusterPage#handleSubmit')
+
   render () {
-    // const { data } = this.props
-    // const cloudProviderOptions = data.map(cp => ({ value: cp.uuid, label: cp.name }))
-    const cloudProviderOptions = [
-      { value: '1', label: 'first cloud provider' },
-      { value: '2', label: 'another cloud provider' },
-    ]
+    const { data=[] } = this.props
+    const cloudProviderOptions = data.map(cp => ({ value: cp.uuid, label: cp.name }))
     return (
       <FormWrapper title="Add Cluster">
         <Wizard onComplete={this.handleSubmit}>
@@ -26,7 +23,6 @@ class AddClusterPage extends React.Component {
                 <WizardStep stepId="type" label="Cluster Type">
                   <ValidatedForm initialValues={wizardContext} onSubmit={setWizardContext} triggerSubmit={onNext}>
                     <PicklistField id="cloudProvider" label="Cloud Provider" options={cloudProviderOptions} />
-                    <h1>Cluster Type</h1>
                   </ValidatedForm>
                 </WizardStep>
                 <WizardStep stepId="config" label="Configuration">
@@ -50,9 +46,6 @@ class AddClusterPage extends React.Component {
   }
 }
 
-compose(
-  withAppContext,
+export default compose(
   withDataLoader({ dataKey: 'cloudProviders', loaderFn: loadCloudProviders }),
 )(AddClusterPage)
-
-export default AddClusterPage
