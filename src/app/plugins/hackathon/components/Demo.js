ecf212d1602b4d8e5c2289d34d53545f75e5cac4
Brennan Cheung <git@brennancheung.com>
2019-02-05 15:49:50 -0800

    Final demo prep

--- src/app/plugins/hackathon/components/Demo.js
+++ src/app/plugins/hackathon/components/Demo.js
@@ -24,20 +24,33 @@ class Demo extends React.Component {
     const { selectedWorkflow, workflows } = this.state
     const workflow = workflows.find(wf => wf.id === selectedWorkflow)
 
+    const picklistStructure = [{ id: 'fieldName', label: 'display text for option' }]
+
     return (
       <div>
         <h1>Demo</h1>
 
-        <h6>TODO: workflow browser / selector</h6>
-        <h6>TODO: workflow save button</h6>
-        <h6>TODO: workflow delete button</h6>
+        <h2>The Task</h2>
+        <p>
+          We want to attach a node to a Kubernetes cluster.  To do this we
+          need to populate a picklist that lets us choose from the list
+          of eligible nodes.
+        </p>
+
+        <p>The format for the Picklist looks like this</p>
+        <pre>{JSON.stringify(picklistStructure, null, 4)}</pre>
+
+        <p>
+          Let's create a workflow to request the nodes from an API and then generate the necessary
+          data structure for the Picklist.
+        </p>
 
         <NodeEditor
           workflow={workflow}
           onSaveWorkflow={this.handleSaveWorkflow}
         />
 
-        <FormBuilder />
+        {false && <FormBuilder />}
       </div>
     )
   }
