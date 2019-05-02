6bbf4725a944d953de23fa6c58c9171834fc0050
Brennan Cheung <git@brennancheung.com>
2019-05-01 18:48:47 -0700

    Preliminary support for theming assistant

--- src/app/plugins/theme/components/ConfigureThemePage.js
+++ src/app/plugins/theme/components/ConfigureThemePage.js
@@ -1,22 +1,59 @@
 import React from 'react'
-import { SketchPicker } from 'react-color'
-
-import { withAppContext } from 'core/AppContext'
+import ColorPicker from './ColorPicker'
+import { Grid } from '@material-ui/core'
+import KitchenSink from './KitchenSink'
+import Panel from './Panel'
 
 class ConfigureThemePage extends React.Component {
   render () {
-    const { context } = this.props
-    const { theme } = context
-
     return (
       <div>
         <h1>Configure Theme</h1>
-        <SketchPicker />
 
-        <pre>{JSON.stringify(theme, null, 4)}</pre>
+        <Grid container spacing={24}>
+          <Grid item xs={8}>
+            <KitchenSink />
+          </Grid>
+          <Grid item xs={4}>
+            <Panel title="Palette">
+              <ColorPicker path="palette.common.white" />
+              <ColorPicker path="palette.common.black" />
+              <br />
+
+              <ColorPicker path="palette.background.paper" />
+              <ColorPicker path="palette.background.default" />
+              <br />
+
+              <ColorPicker path="palette.primary.light" />
+              <ColorPicker path="palette.primary.main" />
+              <ColorPicker path="palette.primary.dark" />
+              <ColorPicker path="palette.primary.contrastText" />
+              <br />
+
+              <ColorPicker path="palette.secondary.light" />
+              <ColorPicker path="palette.secondary.main" />
+              <ColorPicker path="palette.secondary.dark" />
+              <ColorPicker path="palette.secondary.contrastText" />
+              <br />
+
+              <ColorPicker path="palette.error.light" />
+              <ColorPicker path="palette.error.main" />
+              <ColorPicker path="palette.error.dark" />
+              <ColorPicker path="palette.error.contrastText" />
+              <br />
+
+              <ColorPicker path="palette.text.primary" />
+              <ColorPicker path="palette.text.secondary" />
+              <ColorPicker path="palette.text.disabled" />
+              <ColorPicker path="palette.text.hint" />
+              <br />
+            </Panel>
+
+          </Grid>
+        </Grid>
       </div>
     )
   }
 }
 
-export default withAppContext(ConfigureThemePage)
+export default ConfigureThemePage
---
+++ src/app/plugins/theme/components/KitchenSink.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Panel from './Panel'
+import Tab from 'core/components/Tab'
+import Tabs from 'core/components/Tabs'
+import {
+  Button,
+  Typography,
+} from '@material-ui/core'
+import { withAppContext } from 'core/AppContext'
+
+const typographyVariants = [
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+  'subtitle1', 'subtitle2', 'body1', 'body2',
+  'caption', 'button', 'overline'
+]
+
+const KitchenSink = ({ context: { theme } }) => (
+  <div>
+    <Panel title="Buttons">
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">Primary</Button>
+      <Button variant="contained" color="secondary">Secondary</Button>
+    </Panel>
+
+    <Panel title="Tabs">
+      <Tabs>
+        <Tab value="first" label="First one">
+          <Typography variant="subtitle1">First tab contents</Typography>
+        </Tab>
+        <Tab value="second" label="Second">
+          <Typography variant="subtitle1">Second tab contents</Typography>
+        </Tab>
+      </Tabs>
+    </Panel>
+
+    <Panel title="Typography" defaultExpanded={false}>
+      <div>
+        {typographyVariants.map(variant =>
+          <div key={variant}>
+            <Typography variant={variant}>{variant}</Typography>
+            <br />
+          </div>
+        )}
+      </div>
+    </Panel>
+
+    <Panel title="Raw theme JSON" defaultExpanded>
+      <pre>{JSON.stringify(theme, null, 4)}</pre>
+    </Panel>
+  </div>
+)
+
+export default withAppContext(KitchenSink)
