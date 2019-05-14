76f4bbde39bea5022eb92f496a6d9e525f60dcad
Brennan Cheung <git@brennancheung.com>
2019-05-14 13:45:23 -0700

    Group prometheus functions by CRD object type

--- src/app/plugins/kubernetes/components/prometheus/actions.js
+++ src/app/plugins/kubernetes/components/prometheus/actions.js
@@ -3,29 +3,21 @@ import { pathOr, prop, propEq } from 'ramda'
 import contextLoader from 'core/helpers/contextLoader'
 import contextUpdater from 'core/helpers/contextUpdater'
 
-const mapServiceMonitor = ({ clusterUuid, metadata, spec }) => ({
-  clusterUuid,
-  name: metadata.name,
-  namespace: metadata.namespace,
-  labels: metadata.labels,
-  port: spec.endpoints.map(prop('port')).join(', '),
-  selector: spec.selector.matchLabels,
+const mapAsyncItems = async (values, loaderFn, mapFn) => {
+  const promises = values.map(loaderFn)
+  const responses = await Promise.all(promises)
+  const items = responses.flat().map(mapFn)
+  return items
+}
+
+export const loadClusterTags = contextLoader('clusterTags', async ({ apiClient, loadFromContext }) => {
+  await loadFromContext('clusters')
+  return apiClient.appbert.getClusterTags()
 })
 
-const mapRule = ({ clusterUuid, metadata, spec }) => ({
-  clusterUuid,
-  name: metadata.name,
-  namespace: metadata.namespace,
-  labels: metadata.labels,
-})
+const hasMonitoring = cluster => cluster.tags.includes('pf9-system:monitoring')
 
-const mapAlertManager = ({ clusterUuid, metadata, spec }) => ({
-  clusterUuid,
-  name: metadata.name,
-  namespace: metadata.namespace,
-  replicas: spec.replicas,
-  labels: metadata.labels,
-})
+/* Prometheus Instances */
 
 export const mapPrometheusInstance = ({ clusterUuid, metadata, spec }) => ({
   clusterUuid,
@@ -47,51 +39,15 @@ export const mapPrometheusInstance = ({ clusterUuid, metadata, spec }) => ({
   spec,
 })
 
-const mapAsyncItems = async (values, loaderFn, mapFn) => {
-  const promises = values.map(loaderFn)
-  const responses = await Promise.all(promises)
-  const items = responses.flat().map(mapFn)
-  return items
-}
-
-export const loadClusterTags = contextLoader('clusterTags', async ({ apiClient, loadFromContext }) => {
-  await loadFromContext('clusters')
-  return apiClient.appbert.getClusterTags()
-})
-
-const hasMonitoring = cluster => cluster.tags.includes('pf9-system:monitoring')
-
 export const loadPrometheusInstances = contextLoader('prometheusInstances', async ({ apiClient, loadFromContext }) => {
   const clusterTags = await loadFromContext('clusterTags')
   const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
   return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusInstances, mapPrometheusInstance)
 })
 
-export const loadPrometheusRules = contextLoader('prometheusRules', async ({ apiClient, loadFromContext }) => {
-  const clusterTags = await loadFromContext('clusterTags')
-  const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
-  return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusRules, mapRule)
-})
-
-export const loadPrometheusServiceMonitors = contextLoader('prometheusServiceMonitors', async ({ apiClient, loadFromContext }) => {
-  const clusterTags = await loadFromContext('clusterTags')
-  const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
-  return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusServiceMonitors, mapServiceMonitor)
-})
-
-export const loadPrometheusAlertManagers = contextLoader('prometheusAlertManagers', async ({ apiClient, loadFromContext }) => {
-  const clusterTags = await loadFromContext('clusterTags')
-  const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
-  return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusAlertManagers, mapAlertManager)
-})
-
 export const createPrometheusInstance = contextUpdater('prometheusInstances', async ({ data, apiClient, currentItems }) => {
   const createdInstance = await apiClient.qbert.createPrometheusInstance(data.cluster, data)
   return [...currentItems, mapPrometheusInstance({clusterUuid: data.cluster, ...createdInstance})]
-})
-
-export const loadServiceAccounts = contextLoader('serviceAccounts', async ({ apiClient, data }) => {
-  return apiClient.qbert.getServiceAccounts(data.clusterUuid, data.namespace)
 })
 
 export const deletePrometheusInstance = contextUpdater('prometheusInstances', async ({ id, apiClient, currentItems }) => {
@@ -102,6 +58,27 @@ export const deletePrometheusInstance = contextUpdater('prometheusInstances', as
   }
   await apiClient.qbert.deletePrometheusInstance(instance.clusterUuid, instance.namespace, instance.name)
   return currentItems.filter(x => x.id !== id)
+})
+
+/* Service Accounts */
+
+export const loadServiceAccounts = contextLoader('serviceAccounts', async ({ apiClient, data }) => {
+  return apiClient.qbert.getServiceAccounts(data.clusterUuid, data.namespace)
+})
+
+/* Prometheus Rules */
+
+const mapRule = ({ clusterUuid, metadata, spec }) => ({
+  clusterUuid,
+  name: metadata.name,
+  namespace: metadata.namespace,
+  labels: metadata.labels,
+})
+
+export const loadPrometheusRules = contextLoader('prometheusRules', async ({ apiClient, loadFromContext }) => {
+  const clusterTags = await loadFromContext('clusterTags')
+  const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
+  return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusRules, mapRule)
 })
 
 export const deletePrometheusRule = contextUpdater('prometheusRules', async ({ id, currentItems, apiClient }) => {
@@ -115,6 +92,23 @@ export const deletePrometheusRule = contextUpdater('prometheusRules', async ({ i
   return currentItems.filter(x => calcId(x) !== calcId(rule))
 })
 
+/* Service Monitors */
+
+const mapServiceMonitor = ({ clusterUuid, metadata, spec }) => ({
+  clusterUuid,
+  name: metadata.name,
+  namespace: metadata.namespace,
+  labels: metadata.labels,
+  port: spec.endpoints.map(prop('port')).join(', '),
+  selector: spec.selector.matchLabels,
+})
+
+export const loadPrometheusServiceMonitors = contextLoader('prometheusServiceMonitors', async ({ apiClient, loadFromContext }) => {
+  const clusterTags = await loadFromContext('clusterTags')
+  const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
+  return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusServiceMonitors, mapServiceMonitor)
+})
+
 export const deletePrometheusServiceMonitor = contextUpdater('prometheusServiceMonitors', async ({ id, currentItems, apiClient }) => {
   const calcId = x => `${x.clusterUuid}-${x.namespace}-${x.name}`
   const sm = currentItems.find(rule => id === calcId(rule))
@@ -124,6 +118,22 @@ export const deletePrometheusServiceMonitor = contextUpdater('prometheusServiceM
   }
   await apiClient.qbert.deletePrometheusServiceMonitor(sm.clusterUuid, sm.namespace, sm.name)
   return currentItems.filter(x => calcId(x) !== calcId(sm))
+})
+
+/* Alert Managers */
+
+const mapAlertManager = ({ clusterUuid, metadata, spec }) => ({
+  clusterUuid,
+  name: metadata.name,
+  namespace: metadata.namespace,
+  replicas: spec.replicas,
+  labels: metadata.labels,
+})
+
+export const loadPrometheusAlertManagers = contextLoader('prometheusAlertManagers', async ({ apiClient, loadFromContext }) => {
+  const clusterTags = await loadFromContext('clusterTags')
+  const clusterUuids = clusterTags.filter(hasMonitoring).map(prop('uuid'))
+  return mapAsyncItems(clusterUuids, apiClient.qbert.getPrometheusAlertManagers, mapAlertManager)
 })
 
 export const deletePrometheusAlertManager = contextUpdater('prometheusAlertManagers', async ({ id, currentItems, apiClient }) => {
