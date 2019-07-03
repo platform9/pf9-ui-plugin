db5a4d260281137233ab485b7e5348d19e2f1ba5
Brennan Cheung <git@brennancheung.com>
2019-07-03 11:11:11 -0700

    Add theme controls to buttons

--- src/app/plugins/theme/components/examples/ButtonsExample.js
+++ src/app/plugins/theme/components/examples/ButtonsExample.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Panel from '../Panel'
-import { Button } from '@material-ui/core'
+import ExternalLink from 'core/components/ExternalLink'
+import ColorPicker from '../ColorPicker'
+import { Button, Typography } from '@material-ui/core'
 
 const ButtonsExample = ({ expanded = false }) => (
   <Panel title="Buttons" defaultExpanded={expanded}>
     <Button variant="contained">Default</Button>
     <Button variant="contained" color="primary">Primary</Button>
     <Button variant="contained" color="secondary">Secondary</Button>
+
+    <br />
+    <br />
+
+
+    <ColorPicker path="palette.grey.300" />
+    <ColorPicker path="palette.primary.main" />
+    <ColorPicker path="palette.secondary.main" />
+    <ColorPicker path="palette.primary.contrastText" />
+    <ColorPicker path="palette.secondary.contrastText" />
+
+    <br />
+    <br />
+
+    <ExternalLink url="https://material-ui.com/components/buttons/" newWindow >Material-UI Docs</ExternalLink>
+    <br />
+    <ExternalLink url="https://material-ui.com/api/button/" newWindow >Material-UI API</ExternalLink>
   </Panel>
 )
 
