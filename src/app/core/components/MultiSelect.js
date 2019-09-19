d4ef96947f14b4c2bf84efe889915795ef057200
Murilo Machado <murilovmachado@gmail.com>
2019-09-19 09:36:35 -0300

    Fix Lint for MultiSelect.js

--- src/app/core/components/MultiSelect.js
+++ src/app/core/components/MultiSelect.js
@@ -1,78 +1,74 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { makeStyles, createStyles } from '@material-ui/styles';
+import { makeStyles, createStyles } from '@material-ui/styles'
 import Box from '@material-ui/core/Box'
 import FormControl from '@material-ui/core/FormControl'
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import OutlinedInput from '@material-ui/core/OutlinedInput';
-import MenuItem from '@material-ui/core/MenuItem'
-import Select from '@material-ui/core/Select'
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment'
+import OutlinedInput from '@material-ui/core/OutlinedInput'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import SearchIcon from '@material-ui/icons/Search'
 
 const useStyles = makeStyles(theme => createStyles({
-    container: {
-        display: "inline-flex",
-        flexDirection: "column",
-        padding: 10,
-        border: "1px solid #000",
-    },
-    option: {
+  container: {
+    display: 'inline-flex',
+    flexDirection: 'column',
+    padding: 10,
+    border: '1px solid #000',
+  },
+  option: {
 
-    },
-}));
+  },
+}))
 
-const MultiSelect = ({label, options, values, onChange}) => {
-    const classes = useStyles();
+const MultiSelect = ({ label, options, values, onChange }) => {
+  const classes = useStyles()
 
-    const toggleOption = (value) => {
-        const updatedValues = values.includes(value)
-            ? values.filter(currentValue => currentValue !== value)
-            : [...values, value];
+  const toggleOption = (value) => {
+    const updatedValues = values.includes(value)
+      ? values.filter(currentValue => currentValue !== value)
+      : [...values, value]
 
-        onChange(updatedValues);
-    };
+    onChange(updatedValues)
+  }
 
-    return (
-        <Box className={classes.container}>
-            <SearchField />
-            {options.map((option) => (
-                <Option
-                    classes={classes}
-                    key={option.value}
-                    label={option.label}
-                    value={option.value}
-                    checked={values.includes(option.value)}
-                    onChange={() => toggleOption(option.value)}
-                />
-            ))}
-        </Box>
-    );
-};
+  return (
+    <Box className={classes.container}>
+      <SearchField />
+      {options.map((option) => (
+        <Option
+          classes={classes}
+          key={option.value}
+          label={option.label}
+          value={option.value}
+          checked={values.includes(option.value)}
+          onChange={() => toggleOption(option.value)}
+        />
+      ))}
+    </Box>
+  )
+}
 
 const SearchField = () => {
-    const [term, setTerm] = useState("");
+  const [term, setTerm] = useState('')
 
-    return (
-        <FormControl>
-            <OutlinedInput
-                value={term}
-                onChange={(e) => setTerm(e.target.value)}
-                startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
-            />
-        </FormControl>
-    );
-};
+  return (
+    <FormControl>
+      <OutlinedInput
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+        startAdornment={<InputAdornment position="start"><SearchIcon /></InputAdornment>}
+      />
+    </FormControl>
+  )
+}
 
-const Option = ({classes, label, ...checkboxProps}) =>
-    <FormControlLabel
-        className={classes.options}
-        label={label}
-        control={<Checkbox {...checkboxProps} />}
-    />;
+const Option = ({ classes, label, ...checkboxProps }) =>
+  <FormControlLabel
+    className={classes.options}
+    label={label}
+    control={<Checkbox {...checkboxProps} />}
+  />
 
 const optionPropType = PropTypes.shape({
   value: PropTypes.string,
@@ -80,11 +76,10 @@ const optionPropType = PropTypes.shape({
 })
 
 MultiSelect.propTypes = {
-  name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(optionPropType).isRequired,
   values: PropTypes.arrayOf(PropTypes.string),
   onChange: PropTypes.func,
 }
 
-export default MultiSelect;
+export default MultiSelect
--- src/stories/common/MultiSelect.stories.js
+++ src/stories/common/MultiSelect.stories.js
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import { addStoriesFromModule } from '../helpers';
-import MultiSelect from 'core/components/MultiSelect';
+import React, { useState } from 'react'
+import { addStoriesFromModule } from '../helpers'
+import MultiSelect from 'core/components/MultiSelect'
 
 const addStories = addStoriesFromModule(module)
 
 addStories('Common Components/MultiSelect', {
-    'with options': () => {
-        const [values, setValues] = useState(["A"]);
-        const options = [
-            {value: "A", label: "Option A"},
-            {value: "B", label: "Option B"},
-        ];
+  'with options': () => {
+    const [values, setValues] = useState(["A"]);
+    const options = [
+      {value: "A", label: "Option A"},
+      {value: "B", label: "Option B"},
+    ];
 
-        return (
-            <MultiSelect
-                label="Select one or more option"
-                options={options}
-                values={values}
-                onChange={setValues}
-            />
-        );
-    },
+    return (
+      <MultiSelect
+        label="Select one or more option"
+        options={options}
+        values={values}
+        onChange={setValues}
+      />
+    );
+  },
 })
