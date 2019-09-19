b896ca07e95432c7d626014b2437dea9007373fc
Murilo Machado <murilovmachado@gmail.com>
2019-09-19 09:40:06 -0300

    Fix lint for MultiSelect.stories.js

--- src/stories/common/MultiSelect.stories.js
+++ src/stories/common/MultiSelect.stories.js
@@ -6,19 +6,19 @@ const addStories = addStoriesFromModule(module)
 
 addStories('Common Components/MultiSelect', {
   'with options': () => {
-    const [values, setValues] = useState(["A"]);
+    const [values, setValues] = useState(['A'])
     const options = [
-      {value: "A", label: "Option A"},
-      {value: "B", label: "Option B"},
-    ];
+      { value: 'A', label: 'Option A' },
+      { value: 'B', label: 'Option B' },
+    ]
 
     return (
       <MultiSelect
-        label="Select one or more option"
+        label='Select one or more option'
         options={options}
         values={values}
         onChange={setValues}
       />
-    );
+    )
   },
 })
