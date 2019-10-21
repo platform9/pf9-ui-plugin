f249ab4f69bf2909075dd98e20ae6e418d4099a3
Murilo Machado <murilovmachado@gmail.com>
2019-10-21 15:48:57 -0300

    Implement basic step

--- src/app/plugins/kubernetes/components/storage/StorageClassesAddPage.js
+++ src/app/plugins/kubernetes/components/storage/StorageClassesAddPage.js
@@ -2,14 +2,23 @@ import React from 'react'
 import createAddComponents from 'core/helpers/createAddComponents'
 import Wizard from 'core/components/wizard/Wizard'
 import WizardStep from 'core/components/wizard/WizardStep'
+import FormWrapper from 'core/components/FormWrapper'
+import ValidatedForm from 'core/components/validatedForm/ValidatedForm'
+import TextField from 'core/components/validatedForm/TextField'
+import PicklistField from 'core/components/validatedForm/PicklistField'
+import CheckboxField from 'core/components/validatedForm/CheckboxField'
+import ClusterPicklist from 'k8s/components/common/ClusterPicklist'
 import { storageClassesCacheKey } from './actions'
+import useParams from 'core/hooks/useParams'
+
+const initialContext = {}
 
 export const AddStorageClassForm = () => {
   return (
-    <Wizard>
-      {() =>
+    <Wizard context={initialContext}>
+      {({ setWizardContext, onNext }) =>
         <>
-          <BasicStep />
+          <BasicStep onSubmit={setWizardContext} triggerSubmit={onNext} />
           <CustomizeStep />
         </>
       }
@@ -17,8 +26,44 @@ export const AddStorageClassForm = () => {
   )
 }
 
-// TODO: implement Steps
-const BasicStep = () => <WizardStep stepId="basic" label="Basic">Basic Step</WizardStep>
+const BasicStep = ({ onSubmit, triggerSubmit }) => {
+  const { params, getParamsUpdater } = useParams()
+
+  return (
+    <WizardStep stepId="basic" label="Basic">
+      <p>
+        Create a new storage class on a specific cluster by specifying the storage type that maps to the cloud provider for that cluster.
+      </p>
+      <FormWrapper title="Add Storage Class">
+        <ValidatedForm onSubmit={onSubmit} triggerSubmit={triggerSubmit}>
+          <TextField
+            id="name"
+            label="Name"
+            info="Name for this storage class."
+            required
+          />
+          <PicklistField
+            DropdownComponent={ClusterPicklist}
+            id="clusterId"
+            label="Cluster"
+            onChange={getParamsUpdater('clusterId')}
+            info="The cluster to deploy this storage class on."
+            value={params.clusterId}
+            required
+          />
+          <CheckboxField
+            id="isDefault"
+            label="Use as Default Storage Class"
+            onChange={getParamsUpdater('isDefault')}
+            info=""
+          />
+        </ValidatedForm>
+      </FormWrapper>
+    </WizardStep>
+  )
+}
+
+// TODO: implement CustomizeStep
 const CustomizeStep = () => <WizardStep stepId="customize" label="Customize">Customize Step</WizardStep>
 
 export const options = {
