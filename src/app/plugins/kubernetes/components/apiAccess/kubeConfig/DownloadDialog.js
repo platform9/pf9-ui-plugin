58bc2211de7cb044c1cdae16aeebc52e82cb82a6
Murilo Machado <murilovmachado@gmail.com>
2019-11-18 19:01:56 -0300

    Implement username/password auth method

--- src/app/plugins/kubernetes/components/apiAccess/kubeConfig/DownloadDialog.js
+++ src/app/plugins/kubernetes/components/apiAccess/kubeConfig/DownloadDialog.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import ValidatedForm from 'core/components/validatedForm/ValidatedForm'
 import TextField from 'core/components/validatedForm/TextField'
@@ -6,6 +6,9 @@ import { Dialog, DialogTitle, DialogContent, RadioGroup, Radio, Grid, FormContro
 import CancelButton from 'core/components/buttons/CancelButton'
 import SubmitButton from 'core/components/buttons/SubmitButton'
 import { passwordValidator } from 'core/utils/fieldValidators'
+import ApiClient from 'api-client/ApiClient'
+
+const { keystone } = ApiClient.getInstance()
 
 const useStyles = makeStyles(theme => ({
   dialog: {
@@ -25,74 +28,105 @@ const useStyles = makeStyles(theme => ({
 const DownloadDialog = ({ onDownloadClick, onClose, isDialogOpen }) => {
   const classes = useStyles()
   const [authMethod, setAuthMethod] = useState('token')
+  const [note, setNote] = useState()
+  const [confirmLabel, setConfirmLabel] = useState()
+  const [type, setType] = useState()
 
-  const tokenNote = 'Token authentication is the preferred method for downloading kubeconfig. The kubeconfig will remain valid for the next 24 hours.'
-  const passwordNote = 'Password authentication is less secure than token authentication, but the kubeconfig will remain functional for as long as the username and password are valid.'
+  useEffect(() => {
+    if (authMethod === 'token') {
+      setNote('Token authentication is the preferred method for downloading kubeconfig. The kubeconfig will remain valid for the next 24 hours.')
+      setConfirmLabel('Download Config')
+      setType('button')
+    } else {
+      setNote('Password authentication is less secure than token authentication, but the kubeconfig will remain functional for as long as the username and password are valid.')
+      setConfirmLabel('Validate Credentials')
+      setType('submit')
+    }
+  }, [authMethod])
 
-  const tokenConfirm = 'Download Config'
-  const passwordConfirm = 'Validate Credentials'
+  const handleSubmit = async (params) => {
+    const { username, password } = params
+    const token = authMethod === 'token'
+      ? await tokenAuth()
+      : await passwordAuth(username, password)
+
+    if (token) {
+      await onDownloadClick(token)
+    }
+  }
 
   return (
     <Dialog open={isDialogOpen} onClose={onClose}>
       <DialogTitle>Download Kubeconfig</DialogTitle>
       <DialogContent>
-        <Grid container spacing={3}>
-          <Grid item xs={4} zeroMinWidth>
-            <h3>Authentication Method</h3>
-          </Grid>
-          <Grid item xs={8} zeroMinWidth>
-            <RadioGroup
-              value={authMethod}
-              onChange={(event) => setAuthMethod(event.target.value)}
-              className={classes.radioGroup}
-            >
-              <FormControlLabel value="token" label="Token" control={<Radio color="primary" />} />
-              <FormControlLabel value="password" label="Password" control={<Radio color="primary" />} />
-            </RadioGroup>
-          </Grid>
-          <Grid item xs={4} zeroMinWidth>
-            &nbsp;
-          </Grid>
-          <Grid item xs={8} zeroMinWidth>
-            <b>Note: </b>{authMethod === 'token' ? tokenNote : passwordNote}
-          </Grid>
-          {authMethod === 'password' &&
-            <Grid item xs={12} zeroMinWidth>
-              <PasswordForm classes={classes} />
+        <ValidatedForm onSubmit={handleSubmit} fullWidth>
+          <Grid container spacing={3}>
+            <Grid item xs={4} zeroMinWidth>
+              <h3>Authentication Method</h3>
             </Grid>
-          }
-          <Grid item xs={12} zeroMinWidth className={classes.formButtons}>
-            <CancelButton onClick={onClose}>Cancel</CancelButton>
-            <SubmitButton onClick={onDownloadClick}>{authMethod === 'token' ? tokenConfirm : passwordConfirm}</SubmitButton>
+            <Grid item xs={8} zeroMinWidth>
+              <RadioGroup
+                value={authMethod}
+                onChange={(event) => setAuthMethod(event.target.value)}
+                className={classes.radioGroup}
+              >
+                <FormControlLabel value="token" label="Token" control={<Radio color="primary" />} />
+                <FormControlLabel value="password" label="Password" control={<Radio color="primary" />} />
+              </RadioGroup>
+            </Grid>
+            <Grid item xs={4} zeroMinWidth>
+              &nbsp;
+            </Grid>
+            <Grid item xs={8} zeroMinWidth>
+              <b>Note: </b>{note}
+            </Grid>
+            {authMethod === 'password' &&
+              <Grid item xs={12} zeroMinWidth>
+                <PasswordForm
+                  classes={classes}
+                />
+              </Grid>
+            }
+            <Grid item xs={12} zeroMinWidth className={classes.formButtons}>
+              <CancelButton onClick={onClose}>Cancel</CancelButton>
+              <SubmitButton type={type} onClick={handleSubmit}>{confirmLabel}</SubmitButton>
+            </Grid>
           </Grid>
-        </Grid>
+        </ValidatedForm>
       </DialogContent>
     </Dialog>
   )
 }
 
 const PasswordForm = ({ classes }) =>
-  <ValidatedForm fullWidth>
-    <Grid container item xs={12}>
-      <Grid item xs={4} zeroMinWidth>
-        <h3>Username</h3>
-      </Grid>
-      <Grid item xs={8} zeroMinWidth>
-        <TextField id="username" label="username" required />
-      </Grid>
-      <Grid item xs={4} zeroMinWidth>
-        <h3>Password</h3>
-      </Grid>
-      <Grid item xs={8} zeroMinWidth>
-        <TextField
-          id="password"
-          label="password"
-          type="password"
-          validations={[passwordValidator]}
-          required
-        />
-      </Grid>
+  <Grid container item xs={12}>
+    <Grid item xs={4} zeroMinWidth>
+      <h3>Username</h3>
     </Grid>
-  </ValidatedForm>
+    <Grid item xs={8} zeroMinWidth>
+      <TextField id="username" label="username" required />
+    </Grid>
+    <Grid item xs={4} zeroMinWidth>
+      <h3>Password</h3>
+    </Grid>
+    <Grid item xs={8} zeroMinWidth>
+      <TextField
+        id="password"
+        label="password"
+        type="password"
+        validations={[passwordValidator]}
+        required
+      />
+    </Grid>
+  </Grid>
+
+const tokenAuth = async () => keystone.renewScopedToken()
+
+const passwordAuth = async (username, password) => {
+  if (username && password) {
+    const authResult = await keystone.authenticate(username, password)
+    return authResult.unscopedToken
+  }
+}
 
 export default DownloadDialog
