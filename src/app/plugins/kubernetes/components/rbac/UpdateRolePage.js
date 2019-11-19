f98d6748273d05921a1a49e1047b4e514bf51666
Xan Torres <xan.torres@gmail.com>
2019-11-19 17:38:25 +0700

    Fixing rbac UpdateRolePage

--- src/app/plugins/kubernetes/components/rbac/UpdateRolePage.js
+++ src/app/plugins/kubernetes/components/rbac/UpdateRolePage.js
@@ -1,61 +1,52 @@
 import ValidatedForm from 'core/components/validatedForm/ValidatedForm'
 import SubmitButton from 'core/components/SubmitButton'
-import React, { useMemo, useCallback, useEffect } from 'react'
+import React, { useMemo, useCallback } from 'react'
 import useDataUpdater from 'core/hooks/useDataUpdater'
 import { roleActions } from 'k8s/components/rbac/actions'
 import useDataLoader from 'core/hooks/useDataLoader'
 import { emptyObj } from 'utils/fp'
 import useReactRouter from 'use-react-router'
 import { propEq } from 'ramda'
-import useParams from 'core/hooks/useParams'
 import RbacChecklist from './RbacChecklist'
 import FormWrapper from 'core/components/FormWrapper'
 import PresetField from 'core/components/PresetField'
+import { k8sPrefix } from 'app/constants'
+import { pathJoin } from 'utils/misc'
 
-// TODO: get default params for rbac property
-const defaultParams = {
-  rbac: {}
-}
+const backUrl = pathJoin(k8sPrefix, 'rbac')
 
 const UpdateRolePage = () => {
   const { match, history } = useReactRouter()
   const roleId = match.params.id
   const clusterId = match.params.clusterId
   const onComplete = useCallback(
-    success => success && history.push('/ui/kubernetes/rbac'),
+    success => success && history.push(backUrl),
     [history])
-  const [roles] = useDataLoader(roleActions.list, { clusterId })
+  const [roles, loading] = useDataLoader(roleActions.list, { clusterId })
   const role = useMemo(
     () => roles.find(propEq('id', roleId)) || emptyObj,
     [roles, roleId])
-  const [updateRoleAction] = useDataUpdater(roleActions.update, onComplete)
-  const handleSubmit = params => data => updateRoleAction({ ...data, ...params })
-  const { params, updateParams, getParamsUpdater } = useParams(defaultParams)
-  useEffect(() => {
-    updateParams({
-      name: role.name,
-      clusterName: role.clusterName,
-      clusterId: role.clusterId,
-      namespace: role.namespace,
-    })
-  }, [role])
+  const [updateRoleAction, updating] = useDataUpdater(roleActions.update, onComplete)
+  const handleSubmit = useCallback(
+    data => updateRoleAction(({ ...role, ...data })),
+    [role])
 
   return (
     <FormWrapper
       title="Edit Role"
-      backUrl='/ui/kubernetes/rbac'
+      backUrl={backUrl}
+      loading={loading || updating}
+      message={loading ? 'Loading role...' : 'Submitting form...'}
     >
-      <ValidatedForm onSubmit={handleSubmit(params)}>
-        <PresetField label='Name' value={role.name} />
-        <PresetField label='Cluster' value={role.clusterName} />
-        <PresetField label='Namespace' value={role.namespace} />
-        { role.clusterId && <RbacChecklist
+      <ValidatedForm onSubmit={handleSubmit}>
+        <PresetField id="name" label='Name' value={role.name} />
+        <PresetField id="cluster" label='Cluster' value={role.clusterName} />
+        <PresetField id="namespace" label='Namespace' value={role.namespace} />
+        {role.clusterId && <RbacChecklist
           id="rbac"
           clusterId={role.clusterId}
-          onChange={getParamsUpdater('rbac')}
-          value={params.rbac}
           initialRules={role.rules}
-        /> }
+        />}
         <SubmitButton>Update Role</SubmitButton>
       </ValidatedForm>
     </FormWrapper>
