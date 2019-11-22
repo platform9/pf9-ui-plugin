d4f9fd8c48373389b3567e71bd051b5bce4b993c
jruddell <jruddell@pf9-0122.local>
2019-11-22 12:03:12 -0800

    fix build

--- src/app/plugins/kubernetes/components/infrastructure/clusters/actions.js
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/actions.js
@@ -119,7 +119,7 @@ const createBareOSCluster = async (data = {}, loadFromContext) => {
 
   // 1. Get the nodePoolUuid from the nodePools API and look for the pool with name 'defaultPool'
   const nodePools = await qbert.getNodePools()
-  data.nodePoolUuid = nodePools?.find(x => x.name === 'defaultPool').uuid
+  data.nodePoolUuid = nodePools.find(x => x.name === 'defaultPool').uuid
 
   // 2. Create the cluster
   const cluster = await createGenericCluster(body, data, loadFromContext)
