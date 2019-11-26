9231f83861691d84d16b8440845a2aa6df9cfb5a
jruddell <jruddell@pf9-0122.local>
2019-11-25 17:02:58 -0800

    cluster creation stuff

--- src/app/plugins/kubernetes/components/infrastructure/clusters/bareos/ClusterHostChooser.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/bareos/ClusterHostChooser.tsx
@@ -1,38 +1,65 @@
 import React, { forwardRef, useState } from 'react'
-import Progress from 'core/components/progress/Progress'
 import withFormContext from 'core/components/validatedForm/withFormContext'
-import { Checkbox, Table, TableHead, TableCell, TableRow, TableBody, Typography } from '@material-ui/core'
+import {
+  Checkbox,
+  Table,
+  TableHead,
+  TableCell,
+  TableRow,
+  TableBody,
+  Typography,
+  Theme,
+} from '@material-ui/core'
 import { loadNodes } from 'k8s/components/infrastructure/nodes/actions'
 import useDataLoader from 'core/hooks/useDataLoader'
-import PropTypes from 'prop-types'
 import Loading from 'core/components/Loading'
 import { Refresh } from '@material-ui/icons'
 import useInterval from 'core/hooks/useInterval'
 import { makeStyles } from '@material-ui/styles'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles<any, any>((theme: Theme) => ({
   table: {
     border: '2px solid',
-    borderColor: ({ hasError }) => hasError ? theme.palette.error.main : theme.palette.text.disabled
+    borderColor: ({ hasError }) =>
+      hasError ? theme.palette.error.main : theme.palette.text.disabled,
   },
   tableContainer: {
     margin: theme.spacing(2, 0),
   },
   errorText: {
-    color: theme.palette.error.main
-  }
+    color: theme.palette.error.main,
+  },
 }))
 
+interface Props {
+  isMaster: boolean
+  value: string[]
+  excludeList: string[]
+  hasError: boolean
+  errorMessage: string
+  pollForNodes: boolean
+  onChange: (nodes: string[]) => void
+}
+
 // TODO: is forwardRef actually needed here?
-const ClusterHostChooser = forwardRef(
-  ({ isMaster, onChange, value, excludeList, hasError, errorMessage, pollForNodes = false }, ref) => {
+const ClusterHostChooser: React.ComponentType<Props> = forwardRef(({
+    isMaster = false,
+    onChange,
+    value = [],
+    excludeList = [],
+    hasError,
+    errorMessage,
+    pollForNodes = false,
+  },
+  ref,
+) => {
     // TODO: need to figure out a way to do validtion with our system to select 1, 3, or 5 nodes only for masters
     // const isValid = () => !isMaster || [1, 3, 5].includes(value.length)
 
     const { table, tableContainer, errorText } = useStyles({ hasError })
     const allSelected = () => value.length === orphanedNodes.length && value.length > 0
-    const toggleAll = () => onChange(allSelected() ? [] : orphanedNodes.map(x => x.uuid))
-    const isSelected = uuid => value.includes(uuid)
+    const toggleAll = () => onChange(allSelected() ? [] : orphanedNodes.map((x) => x.uuid))
+    const isSelected = (uuid) => value.includes(uuid)
 
     const [nodes, loading, loadMore] = useDataLoader(loadNodes)
 
@@ -52,15 +79,15 @@ const ClusterHostChooser = forwardRef(
     }
     const isLoading = loading || fetchingIn === 0
 
-    const notAssignedToCluster = node => !node.clusterUuid
+    const notAssignedToCluster = (node) => !node.clusterUuid
 
     // exclude list does not filter anything if isMaster
-    const notInExcludeList = node => isMaster || !excludeList.includes(node.uuid)
+    const notInExcludeList = (node) => isMaster || !excludeList.includes(node.uuid)
 
     const orphanedNodes = nodes.filter(notAssignedToCluster).filter(notInExcludeList)
 
-    const toggleHost = uuid => () => {
-      const newHosts = isSelected(uuid) ? value.filter(x => x !== uuid) : [...value, uuid]
+    const toggleHost = (uuid) => () => {
+      const newHosts = isSelected(uuid) ? value.filter((x) => x !== uuid) : [...value, uuid]
       onChange(newHosts)
     }
 
@@ -89,42 +116,36 @@ const ClusterHostChooser = forwardRef(
             </TableRow>
           </TableHead>
           <TableBody>
-            { orphanedNodes.length === 0 && (
-              <TableRow colSpan={4}>
-                <TableCell colSpan={4} align="center"><Typography variant="body1">There are no nodes available.</Typography></TableCell>
+            {orphanedNodes.length === 0 && (
+              <TableRow>
+                <TableCell colSpan={4} align="center">
+                  <Typography variant="body1">There are no nodes available.</Typography>
+                </TableCell>
               </TableRow>
             )}
-            {orphanedNodes.map(orphanedNode => (
+            {orphanedNodes.map((orphanedNode = {}) => (
               <TableRow key={orphanedNode.uuid}>
                 <TableCell>
-                  <Checkbox checked={isSelected(orphanedNode.uuid)} onChange={toggleHost(orphanedNode.uuid)} />
+                  <Checkbox
+                    checked={isSelected(orphanedNode.uuid)}
+                    onChange={toggleHost(orphanedNode.uuid)}
+                  />
                 </TableCell>
                 <TableCell>{orphanedNode.name}</TableCell>
                 <TableCell>{orphanedNode.primaryIp}</TableCell>
-                <TableCell>{orphanedNode.combined.osInfo}</TableCell>
+                <TableCell>{orphanedNode.combined?.osInfo}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
-        { hasError && <Typography variant="body1" className={errorText}>{errorMessage}</Typography> }
+        {hasError && (
+          <Typography variant="body1" className={errorText}>
+            {errorMessage}
+          </Typography>
+        )}
       </div>
     )
   },
 )
 
-ClusterHostChooser.propTypes = {
-  isMaster: PropTypes.bool,
-  onChange: PropTypes.func,
-  value: PropTypes.arrayOf(PropTypes.string),
-
-  // This list will not show up in the choices.  Useful for excluding the already chosen master nodes.
-  excludeList: PropTypes.arrayOf(PropTypes.string),
-}
-
-ClusterHostChooser.defaultProps = {
-  isMaster: false,
-  value: [],
-  excludeList: [],
-}
-
-export default withFormContext(ClusterHostChooser)
+export default withFormContext(ClusterHostChooser) as React.FC<Props>
