9f8bdbb6a14dca517ba74b209cfccfb17f4cb796
Rahul Kurumkar <8025925+rahulkurumkar@users.noreply.github.com>
2019-12-12 14:26:02 +0530

    PMK-2305: Added help page.

---
+++ src/app/plugins/kubernetes/components/common/HelpPage.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Typography, Divider } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import ExternalLink from 'core/components/ExternalLink'
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+  divider: {
+    marginBottom: theme.spacing(2),
+  },
+  text: {
+    margin: theme.spacing(2),
+  },
+}))
+
+const HelpPage = () => {
+  const classes = useStyles()
+  return (
+    <>
+      <Typography variant="h5" className={classes.title}>
+        Support
+      </Typography>
+      <Divider />
+      <Typography variant="subtitle1" className={classes.text}>
+        Need help? You are currently using the free tier account of Platform9 Managed Kubernetes
+        (PMK). There are two ways to get help free tier account:
+      </Typography>
+      <Typography variant="h5">Documentation & Knowledge Base</Typography>
+      <Typography variant="subtitle1" className={classes.text}>
+        Use our documentation and knowledge base to get help wrt various product features and
+        capabilities:
+      </Typography>
+      <Typography variant="subtitle1" className={classes.text}>
+        Reference the PMK documentation at{' '}
+        <ExternalLink url="https://docs.platform9.com/kubernetes/overview/">
+          https://docs.platform9.com/kubernetes/overview/
+        </ExternalLink>
+      </Typography>
+      <Typography variant="h5">Support</Typography>
+      <Typography variant="subtitle1" className={classes.text}>
+        Use our public slack channel to ask questions. The channel url is -{' '}
+        <ExternalLink url="https://kplane.slack.com">kplane.slack.com</ExternalLink>
+      </Typography>
+      <Typography variant="subtitle1" className={classes.text}>
+        You should have received an invite to join the channel already. If you haven't, send an
+        request to <a href="mailto:support-ft@platform9.com">support-ft@platform9.com</a>
+      </Typography>
+    </>
+  )
+}
+
+export default HelpPage
