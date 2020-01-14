ba38ee95e45bc0cf9cd1c79433dea4f25582844a
Rahul Kurumkar <8025925+rahulkurumkar@users.noreply.github.com>
2020-01-14 12:42:45 +0530

    PMK-2485: Refactored the Reset password page also added show password and typescript feature (#609)
    * PMK-2485: Refactored the Reset password page also added show password and typescript feature

--- src/api-client/Clemency.ts
+++ src/api-client/Clemency.ts
@@ -1,42 +1,48 @@
 import config from '../../config'
 import ApiService from 'api-client/ApiService'
 
+export interface IApiData {
+  username: string
+  password: string
+  secret: string
+}
+
+type IResetPassword = ({ secret, username, password }: IApiData) => Promise<any>
+
 class Clemency extends ApiService {
-  endpoint () {
+  endpoint (): string {
     return config.apiHost
   }
 
-  get baseUrl () {
+  get baseUrl (): string {
     return `${this.endpoint()}/clemency`
   }
 
-  requestNewPassword = async (username) => {
+  requestNewPassword = async (username: string): Promise<any> => {
     const body = { username }
     return this.client.basicPost(`${this.baseUrl}/request`, body)
   }
 
-  verifyPasswordReset = async (secret) => {
+  verifyPasswordReset = async (secret: string): Promise<any> => {
     return this.client.basicGet(`${this.baseUrl}/reset/password/${secret}`)
   }
 
-  resetPassword = async (secret, username, password) => {
-    const body = {
-      username, password,
-    }
+  resetPassword: IResetPassword = async ({ secret, username, password }) => {
+    const body = { username, password }
     return this.client.basicPost(`${this.baseUrl}/reset/password/${secret}`, body)
   }
 
-  createUser = async body => {
+  createUser = async (body): Promise<any> => {
     const response = await this.client.basicPost(`${this.baseUrl}/createacct`, body)
     return response.user
   }
 
   // this url format is strange but is a requirement for the API
-  verifyActivateLink = async (username, otp) => {
+  verifyActivateLink = async (username, otp): Promise<any> => {
     return this.client.basicGet(`${this.baseUrl}/activate/username=${username}&otp=${otp}`)
   }
 
-  updateUser = async body => {
+  updateUser = async (body): Promise<any> => {
     return this.client.basicPost(`${this.baseUrl}/updateuser`, body)
   }
 }
