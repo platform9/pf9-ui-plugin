e4db850e11647d602438c3bcecb73ec210b29cdf
Matthew Roark <matthew.roark@platform9.com>
2020-01-21 15:40:46 -0600

    [PMK-2514] Help page improvements
    * Add Swiftype/Elastic Search integration
    * Remove prior text in favor of search and navigational items
    * Add background image to search for theming

--- src/app/plugins/kubernetes/components/common/HelpPage.js
+++ src/app/plugins/kubernetes/components/common/HelpPage.js
@@ -1,52 +1,112 @@
 import React from 'react'
-import { Typography, Divider } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
-import ExternalLink from 'core/components/ExternalLink'
-import SimpleLink from 'core/components/SimpleLink'
 
-const useStyles = makeStyles((theme) => ({
-  title: {
-    marginBottom: theme.spacing(2),
+import SiteSearchAPIConnector from '@elastic/search-ui-site-search-connector'
+import {
+  ErrorBoundary,
+  SearchProvider,
+  SearchBox,
+  Results,
+  PagingInfo,
+  Paging,
+  WithSearch
+} from '@elastic/react-search-ui'
+
+import { Layout } from '@elastic/react-search-ui-views'
+import '@elastic/react-search-ui-views/lib/styles/styles.css'
+import 'app/search.css'
+
+const connector = new SiteSearchAPIConnector({
+  documentType: 'page',
+  engineKey: 'e4e3Eb9y73dFjykKY8uJ',
+  engineName: 'platform9-docs'
+})
+
+const config = {
+  alwaysSearchOnInitialLoad: false,
+  initialState: {
+    resultsPerPage: 5
   },
-  divider: {
-    marginBottom: theme.spacing(2),
+  searchQuery: {
+    result_fields: {
+      title: { snippet: { size: 100, fallback: true } },
+      body: { snippet: { size: 100, fallback: true } },
+      url: { raw: {} }
+    }
   },
-  text: {
-    margin: theme.spacing(2),
-  },
-}))
+  apiConnector: connector
+}
+
+const Article = ({ result: article }) => {
+  console.log(article)
+  return (
+    <div className="st-ui-injected-content st-search-results">
+      <div className="st-query-present">
+        <a href={article.url.raw} className="st-ui-result">
+          <span className="st-ui-type-heading">
+            {
+              <div
+                dangerouslySetInnerHTML={{ __html: article.title.snippet }}
+              />
+            }
+          </span>
+          <span className="st-ui-type-detail">
+            <div dangerouslySetInnerHTML={{ __html: article.body.snippet }} />
+          </span>
+        </a>
+      </div>
+    </div>
+  )
+}
 
 const HelpPage = () => {
-  const classes = useStyles()
   return (
     <>
-      <Typography variant="h5" className={classes.title}>
-        Support
-      </Typography>
-      <Divider />
-      <Typography variant="subtitle1" className={classes.text}>
-        Need help?  You are currently using the free-tier account of Platform9 Managed Kubernetes (PMK).
-        There are 2 ways to get help free tier account:
-      </Typography>
+      <section className={'hero'}>
+        <div className={'hero-welcome'}>
+          <h1>Platform9 Help Center</h1>
+          <h2>You are currently using Platform9 Managed Kubernetes (PMK) <b>free-tier</b>.</h2>
+          <h3>Support for free-tier is limited to our knowledge base and community support via our
+            Slack channel.</h3>
 
-      <Typography variant="h5">Documentation & Knowledge Base</Typography>
-      <Typography variant="subtitle1" className={classes.text}>
-        Use our documentation and knowledge base to get help with various product features and capabilities.
-      </Typography>
-      <Typography variant="subtitle1" className={classes.text}>
-        Reference the PMK documentation at
-        &nbsp;<ExternalLink url="https://docs.platform9.com/kubernetes/overview/">https://docs.platform9.com/kubernetes/overview/</ExternalLink>
-      </Typography>
-
-      <Typography variant="h5">Support</Typography>
-      <Typography variant="subtitle1" className={classes.text}>
-        Use our public slack channel to ask questions. The channel url is:
-        &nbsp;<ExternalLink url="https://kplane.slack.com">kplane.slack.com</ExternalLink>
-      </Typography>
-      <Typography variant="subtitle1" className={classes.text}>
-        You should have received an invite to join the channel already.  If you haven't, send a
-        request to <SimpleLink href="mailto:support-ft@platform9.com">support-ft@platform9.com</SimpleLink>
-      </Typography>
+          <SearchProvider config={config}>
+            <WithSearch mapContextToProps={({ wasSearched }) => ({ wasSearched })}>
+              {({ wasSearched }) => {
+                return (
+                  <div className="App">
+                    <ErrorBoundary>
+                      <Layout
+                        header={
+                          <SearchBox
+                            autocompleteMinimumCharacters={3}
+                            autocompleteResults={{
+                              linkTarget: '_blank',
+                              sectionTitle: 'Results',
+                              titleField: 'title',
+                              urlField: 'nps_link',
+                              shouldTrackClickThrough: true
+                            }}
+                            autocompleteSuggestions
+                            debounceLength={0}
+                          />
+                        }
+                        bodyContent={
+                          <Results titleField="title" resultView={Article} />
+                        }
+                        bodyHeader={
+                          <React.Fragment>
+                            {wasSearched && <PagingInfo />}
+                          </React.Fragment>
+                        }
+                        bodyFooter={<Paging />}
+                      />
+                    </ErrorBoundary>
+                  </div>
+                )
+              }}
+            </WithSearch>
+          </SearchProvider>
+        </div>
+      </section>
     </>
   )
 }
