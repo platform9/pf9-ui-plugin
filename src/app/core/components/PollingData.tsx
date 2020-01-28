e66d9ae2b2d0d9f132febafdd44354a5fb49eb4b
John Ruddell <johnruddell@gmail.com>
2020-01-28 01:31:50 -0800

    refactord the data poller into a component, made the converging nodes page pull data live

---
+++ src/app/core/components/PollingData.tsx
@@ -0,0 +1,60 @@
+// Libs
+import React, { useState, useCallback } from 'react'
+import moment from 'moment'
+import { Typography } from '@material-ui/core'
+import { TypographyProps } from '@material-ui/core/Typography'
+
+// Components
+import Loading from 'core/components/Loading'
+import useInterval from 'core/hooks/useInterval'
+
+interface Props<T> {
+  loading: boolean
+  pause?: boolean
+  pollIntervalMs?: number
+  refreshDuration?: number
+  textColor?: TypographyProps['color']
+  onReload: (ignoreCache?: boolean) => T[]
+}
+
+const defaultRefreshDuration = 1000 * 60 * 5
+
+function PollingData<T> ({
+  loading,
+  onReload,
+  pause = false,
+  pollIntervalMs = 5000,
+  refreshDuration = defaultRefreshDuration,
+  textColor = 'textSecondary',
+}: Props<T>) {
+  const [lastIntervalTs, setLastIntervalTs] = useState(new Date().valueOf())
+  const [lastFetchTs, setLastFetchTs] = useState(new Date().valueOf())
+  const reload = useCallback(() => {
+    const ts = new Date().valueOf()
+    setLastFetchTs(ts)
+    setLastIntervalTs(ts)
+    onReload(true)
+  }, [setLastFetchTs, setLastIntervalTs, onReload])
+
+  if (!pause) {
+    useInterval(() => {
+      if (!loading) {
+        setLastIntervalTs(new Date().valueOf())
+      }
+    }, pollIntervalMs)
+    const currentTs = new Date().valueOf()
+    if ((currentTs - lastFetchTs) > refreshDuration) {
+      reload()
+    }
+  }
+
+  return (
+    <Loading loading={loading} justify="flex-start" onClick={pause ? undefined : reload} reverse>
+      <Typography variant="caption" color={textColor}>
+        {loading ? 'loading...' : moment(lastIntervalTs).fromNow()}
+      </Typography>
+    </Loading>
+  )
+}
+
+export default PollingData
