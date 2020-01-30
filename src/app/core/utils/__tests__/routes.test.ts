16cc1f5bf8a2de9b278b9e1cf6924f8fb21a77f3
John Ruddell <johnruddell@gmail.com>
2020-01-30 00:36:31 -0800

    update

--- src/app/core/utils/__tests__/routes.test.ts
+++ src/app/core/utils/__tests__/routes.test.ts
@@ -1,17 +1,6 @@
 import { Route } from '../routes'
 
-// const list = new Route(`/infrastructure#clusters`)
-// console.log(list.path()) // good to go
-// console.log(list.path({})) // good to go
-// console.log(list.path({search: 'asdf'})) // good to go
-
-// const edit = new Route<{ id: string }>(`/infrastructure/clusters/edit/:id`)
-// console.log(edit.path()) // should error here. id is required
-// console.log(edit.path({})) // should error here. id is required
-// console.log(edit.path({ search: 'asdf' })) // should error here. id is required
-// console.log(edit.path({ id: '1', search: 'asdf'})) // good to go
-
-describe('Route class', () => {
+describe('Route', () => {
   test('url', () => {
     const route = new Route('/ui/kubernetes/infrastructure#clusters')
     expect(route.url).toBe('/ui/kubernetes/infrastructure#clusters')
@@ -24,6 +13,6 @@ describe('Route class', () => {
     expect(route.url).toBe('/ui/kubernetes/infrastructure#clusters')
     expect(route['path' as any]()).toBe('/ui/kubernetes/infrastructure/clusters/:id/edit')
     expect(route.path({ id: 'cluster-uuid' })).toBe('/ui/kubernetes/infrastructure/clusters/cluster-uuid/edit')
-    expect(route.path({ id: 'cluster-uuid', name: 'My Awesome Cluster' })).toBe('/ui/kubernetes/infrastructure/clusters/cluster-uuid/edit')
+    expect(route.path({ id: 'cluster-uuid', name: 'My Awesome Cluster' })).toBe('/ui/kubernetes/infrastructure/clusters/cluster-uuid/edit?name=My+Awesome+Cluster')
   })
 })
