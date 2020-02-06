d6b9779eabfe052497ad286d69993a3706fa6051
Rahul Kurumkar <8025925+rahulkurumkar@users.noreply.github.com>
2020-02-05 18:05:34 -0800

    PMK-2605: Added the max with to node onboard page.

--- src/app/plugins/kubernetes/components/infrastructure/nodes/DownloadCliPage.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/nodes/DownloadCliPage.tsx
@@ -12,6 +12,9 @@ import SimpleLink from 'core/components/SimpleLink'
 import { routes } from 'core/utils/routes'
 
 const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    maxWidth: '850px',
+  },
   spacer: {
     height: theme.spacing(2),
     width: theme.spacing(2),
@@ -23,48 +26,50 @@ const useStyles = makeStyles((theme: Theme) => ({
 }))
 
 const DownloadCliPage: FunctionComponent = () => {
-  const { spacer, backLink } = useStyles({})
+  const { spacer, backLink, root } = useStyles({})
   return (
-    <PageContainer
-      header={
-        <SimpleLink src={routes.nodes.list.path()} className={backLink}>
-          « Back to Node List
-        </SimpleLink>
-      }
-    >
-      <Typography variant="h5">Onboard a new node</Typography>
-      <p> </p>
-      <Typography component="span">
-        In order to add a physical or virtual node to your BareOS cluster, you need to first
-        download and install the Platform9 CLI on that node. Follow the instructions below to
-        download and install the CLI on your node
-      </Typography>
-      <DownloadCliOnboardNodeWalkthrough />
+    <div className={root}>
+      <PageContainer
+        header={
+          <SimpleLink src={routes.nodes.list.path()} className={backLink}>
+            « Back to Node List
+          </SimpleLink>
+        }
+      >
+        <Typography variant="h5">Onboard a new node</Typography>
+        <p> </p>
+        <Typography component="span">
+          In order to add a physical or virtual node to your BareOS cluster, you need to first
+          download and install the Platform9 CLI on that node. Follow the instructions below to
+          download and install the CLI on your node
+        </Typography>
+        <DownloadCliOnboardNodeWalkthrough />
 
-      <p className={spacer} />
-      <Typography variant="h6">CLI Advanced Options</Typography>
-      <p> </p>
-      <Typography component="p" variant="subtitle2">
-        Create clusters and more directly using the CLI
-      </Typography>
-      <p> </p>
-      <Typography component="span" variant="body1">
-        You can use the{' '}
-        <CopyToClipboard copyText="pf9ctl">
-          <CodeBlock>pf9ctl</CodeBlock>
-        </CopyToClipboard>{' '}
-        CLI directly to use one or more PMK clusters. Type{' '}
-        <CopyToClipboard copyText="pf9ctl --help">
-          <CodeBlock>pf9ctl --help</CodeBlock>
-        </CopyToClipboard>{' '}
-        to see the full features and options the CLI supports
-      </Typography>
-      <p> </p>
-      {/* <Typography variant="body1">
+        <p className={spacer} />
+        <Typography variant="h6">CLI Advanced Options</Typography>
+        <p> </p>
+        <Typography component="p" variant="subtitle2">
+          Create clusters and more directly using the CLI
+        </Typography>
+        <p> </p>
+        <Typography component="span" variant="body1">
+          You can use the{' '}
+          <CopyToClipboard copyText="pf9ctl">
+            <CodeBlock>pf9ctl</CodeBlock>
+          </CopyToClipboard>{' '}
+          CLI directly to use one or more PMK clusters. Type{' '}
+          <CopyToClipboard copyText="pf9ctl --help">
+            <CodeBlock>pf9ctl --help</CodeBlock>
+          </CopyToClipboard>{' '}
+          to see the full features and options the CLI supports
+        </Typography>
+        <p> </p>
+        {/* <Typography variant="body1">
         See <SimpleLink src="">CLI Documentation</SimpleLink> for more info on whats supported with the
         CLI
       </Typography> */}
-    </PageContainer>
+      </PageContainer>
+    </div>
   )
 }
 export default DownloadCliPage
