02981a743e1b17e7ce3cada665169bd2655d2a34
xanto <xan.torres@toptal.com>
2020-02-07 01:07:28 +0700

    Set default role names for admin and self-service users

--- src/app/plugins/kubernetes/components/userManagement/roles/actions.js
+++ src/app/plugins/kubernetes/components/userManagement/roles/actions.js
@@ -15,7 +15,9 @@ export const mngmRoleActions = createCRUDActions(mngmRolesCacheKey, {
       filterIf(!params.allRoles, (role) => ['admin', '_member_'].includes(role.name)),
       map((role) => ({
         ...role,
-        name: role.displayName || role.name,
+        name: role.displayName || ['admin', '_member_'].includes(role.name)
+          ? hardcodedKubeRolesNames[role.name]
+          : role.name,
         description: ['admin', '_member_'].includes(role.name)
           ? hardcodedKubeRolesDescriptions[role.name]
           : role.description,
@@ -33,4 +35,9 @@ const hardcodedKubeRolesDescriptions = {
     'An Administrator has global access to all regions, tenants and all resources within your Platform9 environment, and access to perform all operations. As a Platform9 Managed Kubernetes (PMK) administrator you can invite more users to your PMK cloud environment, create or delete tenants, create or delete clusters, deploy workloads on clusters, assign RBAC policies to self-service users so they can access clusters,  etc.',
   _member_:
     'A self-service user has limited access to the Platform9 Managed Kubernetes (PMK) cloud environment. When an Administrator invites a self-service user to the PMK cloud environment, they can start by assigning the user to one or more tenants. The user will only be able to log into those tenants. Within the tenant, a self-service user will by default not have access to any clusters created in that tenant. The administrator will need to create specific RBAC policies to give the user access to one or more clusters within the tenant, before the users can perform any operations on the clusters.',
+}
+
+const hardcodedKubeRolesNames = {
+  admin: 'Administrator',
+  _member_: 'Self-service User',
 }
