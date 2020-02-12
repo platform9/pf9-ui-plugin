b92d87e59d0e6b536d8c06ceb76a49d259d4a20e
jruddell <jruddell@jruddell-pf9.local>
2020-02-12 13:35:34 -0800

    fixed the typings so we now require values that the chart needs.

--- src/app/core/components/graphs/StackedAreaChart.tsx
+++ src/app/core/components/graphs/StackedAreaChart.tsx
@@ -3,55 +3,76 @@ import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 import { useTheme } from '@material-ui/core/styles'
 import { path } from 'ramda'
 
-// Usage:
-// const exampleData = [
-//   {
-//     time: '8:00',
-//     warning: 5,
-//     critical: 10,
-//     fatal: 15,
-//   },
-//   ...
-// ]
+/*
+  Usage:
+  type IRequiredAreaChartTypes = 'warning' | 'critical' | 'fatal'
 
-// const exampleTypes = [
-//   {
-//     name: 'warning',
-//     color: 'warning',
-//   },
-//   ...
-// ]
+  const exampleData = [
+    {
+      time: '8:00',
+      warning: 5,
+      critical: 10,
+      fatal: 15,
+    },
+    ...
+  ]
 
-// const exampleAxis = 'time'
+  const exampleTypes = [
+    {
+      name: 'warning',
+      color: 'warning',
+    },
+    ...
+  ]
 
-export interface AreaChartEntry {
-  [propName: string]: any
-}
+  const exampleAxis = 'time'
 
-export interface AreaChartType {
-  name: string
+  <StackedAreaChart<'time', IRequiredAreaChartTypes>
+    data={exampleData}
+    types={exampleTypes}
+    xAxis="time"
+  />
+
+*/
+
+type AreaChartEntry<T extends string, V extends string> = { [P in T | V]: any }
+
+export interface AreaChartType<T> {
+  name: T
   color: string
 }
 
 // types should be a list of strings that also show up in AreaChartEntry as a property
-interface Props {
-  data: AreaChartEntry[]
-  width: number
-  height: number
-  xAxis: string
-  types: AreaChartType[]
+interface Props<T extends string, V extends string> {
+  data: Array<AreaChartEntry<T, V>>
+  width?: number
+  height?: number
+  xAxis: T
+  types: Array<AreaChartType<V>>
 }
 
 // Todo: Instead of a set width and height, also allow for percents
-const StackedAreaChart = ({ data, width=600, height=400, types, xAxis, ...rest }: Props) => {
+function StackedAreaChart<Axis extends string, Types extends string>({
+  data,
+  width = 600,
+  height = 400,
+  types,
+  xAxis,
+  ...rest
+}: Props<Axis, Types>) {
   const theme: any = useTheme()
 
   return (
-    <AreaChart width={width} height={height} data={data} margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-      <CartesianGrid strokeDasharray="3 3"/>
-      <XAxis dataKey={xAxis}/>
-      <YAxis/>
-      <Tooltip/>
+    <AreaChart
+      width={width}
+      height={height}
+      data={data}
+      margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey={xAxis} />
+      <YAxis />
+      <Tooltip />
       {types.map(({ name, color }) => (
         <Area
           key={name}
