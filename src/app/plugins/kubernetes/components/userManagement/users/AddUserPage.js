4254e111f7b0b3952720003b3c79228db1f6fbdb
xanto <xan.torres@toptal.com>
2020-02-24 18:04:52 +0700

    Revert "Allow selecting multiple tenants for every selected role when adding/editing an user"

--- src/app/plugins/kubernetes/components/userManagement/users/AddUserPage.js
+++ src/app/plugins/kubernetes/components/userManagement/users/AddUserPage.js
@@ -9,13 +9,16 @@ import useReactRouter from 'use-react-router'
 import useDataUpdater from 'core/hooks/useDataUpdater'
 import { k8sPrefix } from 'app/constants'
 import { pathJoin } from 'utils/misc'
+import useDataLoader from 'core/hooks/useDataLoader'
 import TenantRolesTableField from 'k8s/components/userManagement/users/TenantRolesTableField'
 import { mngmUserActions } from 'k8s/components/userManagement/users/actions'
+import Progress from 'core/components/progress/Progress'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 import Radio from '@material-ui/core/Radio'
+import { mngmTenantActions } from 'k8s/components/userManagement/tenants/actions'
 import UserPasswordField from 'k8s/components/userManagement/users/UserPasswordField'
 
 const listUrl = pathJoin(k8sPrefix, 'user_management#users')
@@ -31,6 +34,7 @@ const AddUserPage = () => {
   const { history } = useReactRouter()
   const onComplete = useCallback((success) => success && history.push(listUrl), [history])
   const [handleAdd, submitting] = useDataUpdater(mngmUserActions.create, onComplete)
+  const [tenants, loadingTenants] = useDataLoader(mngmTenantActions.list)
   const [activationType, setActivationType] = useState('createPassword')
 
   const activationByEmailLabel = (
@@ -104,7 +108,13 @@ const AddUserPage = () => {
                 onSubmit={setWizardContext}
                 triggerSubmit={onNext}
               >
-                <TenantRolesTableField required id="roleAssignments" />
+                <Progress
+                  renderContentOnMount={!loadingTenants}
+                  loading={loadingTenants}
+                  message={'Loading Tenants...'}
+                >
+                  <TenantRolesTableField required id="roleAssignments" tenants={tenants} />
+                </Progress>
               </ValidatedForm>
             </WizardStep>
           </>
