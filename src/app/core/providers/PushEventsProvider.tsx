4b34699b0992bdcaa5cb540e3d535b6ad7270677
Brennan Cheung <git@brennancheung.com>
2020-02-24 17:05:12 -0800

    switch to FC so we can useDataUpdater

--- src/app/core/providers/PushEventsProvider.tsx
+++ src/app/core/providers/PushEventsProvider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC, useEffect } from 'react'
 import PushEventManager from 'core/PushEventManager'
 
 interface IMessage {
@@ -7,27 +7,29 @@ interface IMessage {
   payload: any
 }
 
-class PushEventsProvider extends React.Component<{}, {}> {
-  unsubscribe: () => void
+interface Props {
+  children: any
+}
 
+const PushEventsProvider: FC<Props> = ({ children }) => {
   // All this will probably change once we switch to a redux architecture
   // but I am keeping all event handling inside this component for now
   // so that the event handlers have access to the data store and to
   // display toast notifications.
 
-  handleClusterNode = (payload) => {
-    // console.log('qbert::cluster_node', payload)
+  const handleClusterNode = (payload) => {
+    console.log('qbert::cluster_node', payload)
   }
 
-  handleClusterUpdate = (payload) => {
-    // console.log('qbert::cluster_update', payload)
+  const handleClusterUpdate = (payload) => {
+    console.log('qbert::cluster_update', payload)
   }
 
-  handleClusterStatus = (payload) => {
+  const handleClusterStatus = (payload) => {
     const { action } = payload
     const actionHandlers = {
-      create: this.handleClusterStatusCreate(payload),
-      delete: this.handleClusterStatusDelete(payload),
+      create: handleClusterStatusCreate(payload),
+      delete: handleClusterStatusDelete(payload),
     }
     const catchAll = (payload) => {
       console.info(`No push event handler found for qbert::cluster_status with action: ${action}`)
@@ -36,26 +38,26 @@ class PushEventsProvider extends React.Component<{}, {}> {
     handler(payload)
   }
 
-  handleClusterStatusCreate = (payload) => {
+  const handleClusterStatusCreate = (payload) => {
     const clusterUuid = payload.source
     console.log(`Cluster created with uuid: ${clusterUuid}`)
     // TODO update the data store
   }
 
-  handleClusterStatusDelete = (payload) => {
+  const handleClusterStatusDelete = (payload) => {
     const clusterUuid = payload.source
     console.log(`Cluster deleted with uuid: ${clusterUuid}`)
     // TODO update the data store
   }
 
-  handleMessage = (message: IMessage) => {
+  const handleMessage = (message: IMessage) => {
     const { source, type, payload } = message
 
     const handlers = {
       qbert: {
-        cluster_node: this.handleClusterNode,
-        cluster_update: this.handleClusterUpdate,
-        cluster_status: this.handleClusterStatus,
+        cluster_node: handleClusterNode,
+        cluster_update: handleClusterUpdate,
+        cluster_status: handleClusterStatus,
       },
     }
 
@@ -71,19 +73,15 @@ class PushEventsProvider extends React.Component<{}, {}> {
 
     typeHandler(payload)
   }
-
-  componentDidMount = () => {
+  useEffect(() => {
     const pushInstance = PushEventManager.getInstance()
     pushInstance.connect()
-    const unsub = pushInstance.subscribe(this.handleMessage)
-    this.unsubscribe = unsub
-  }
-
-  componentWillUnmount = () => {
-    this.unsubscribe()
-  }
-
-  render = () => this.props.children
+    const unsub = pushInstance.subscribe(handleMessage)
+    return () => {
+      unsub()
+    }
+  }, [])
+  return <>{children}</>
 }
 
 export default PushEventsProvider
