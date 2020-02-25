1524b6a75e293fa05f8f55a050d0abd6a60746e1
Brennan Cheung <git@brennancheung.com>
2020-02-24 16:05:41 -0800

    pull out the cluster create and delete actions into handlers

--- src/app/core/providers/PushEventsProvider.tsx
+++ src/app/core/providers/PushEventsProvider.tsx
@@ -8,7 +8,7 @@ interface IMessage {
 }
 
 class PushEventsProvider extends React.Component<{}, {}> {
-  unsubscribe : () => void
+  unsubscribe: () => void
 
   // All this will probably change once we switch to a redux architecture
   // but I am keeping all event handling inside this component for now
@@ -16,11 +16,36 @@ class PushEventsProvider extends React.Component<{}, {}> {
   // display toast notifications.
 
   handleClusterNode = (payload) => {
-    console.log('qbert::cluster_node', payload)
+    // console.log('qbert::cluster_node', payload)
   }
 
   handleClusterUpdate = (payload) => {
-    console.log('qbert::cluster_update', payload)
+    // console.log('qbert::cluster_update', payload)
+  }
+
+  handleClusterStatus = (payload) => {
+    const { action } = payload
+    const actionHandlers = {
+      create: this.handleClusterStatusCreate(payload),
+      delete: this.handleClusterStatusDelete(payload),
+    }
+    const catchAll = (payload) => {
+      console.info(`No push event handler found for qbert::cluster_status with action: ${action}`)
+    }
+    const handler = actionHandlers[action] || catchAll
+    handler(payload)
+  }
+
+  handleClusterStatusCreate = (payload) => {
+    const clusterUuid = payload.source
+    console.log(`Cluster created with uuid: ${clusterUuid}`)
+    // TODO update the data store
+  }
+
+  handleClusterStatusDelete = (payload) => {
+    const clusterUuid = payload.source
+    console.log(`Cluster deleted with uuid: ${clusterUuid}`)
+    // TODO update the data store
   }
 
   handleMessage = (message: IMessage) => {
@@ -30,7 +55,8 @@ class PushEventsProvider extends React.Component<{}, {}> {
       qbert: {
         cluster_node: this.handleClusterNode,
         cluster_update: this.handleClusterUpdate,
-      }
+        cluster_status: this.handleClusterStatus,
+      },
     }
 
     const sourceHandler = handlers[source]
@@ -56,7 +82,7 @@ class PushEventsProvider extends React.Component<{}, {}> {
   componentWillUnmount = () => {
     this.unsubscribe()
   }
-  
+
   render = () => this.props.children
 }
 
