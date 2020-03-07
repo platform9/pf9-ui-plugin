a728b7557d7280d297265646e92a8e66a5f2de66
kevinylee1991 <kevinylee1991@gmail.com>
2020-03-06 22:28:06 -0500

    Monitoring dashboard page (#726)
    * start of monitoring page
    
    * First pass at alarms dashboard
    
    * Added refresh button for chart, invalidate time series cache, and use prometheus cluster filter
    
    * PR comment fixes
    
    * picklist typescript workaround
    
    * Added grafana link and fixed issue where area chart graph reset when changing filters for table below it
    
    * Changed prometheus API to use ro use rules API instead of alerts API, fixed grafana link, added more to indexBy
    
    * Fix indexBy for timeseries alerts
    
    * PR comment fixes
    
    Co-authored-by: John Ruddell <johnruddell@gmail.com>

---
+++ src/app/plugins/kubernetes/components/alarms/SeverityPicklist.tsx
@@ -0,0 +1,53 @@
+import React, { forwardRef, useEffect } from 'react'
+import { propOr, head } from 'ramda'
+import { allKey } from 'app/constants'
+import PicklistDefault from 'core/components/Picklist'
+const Picklist: any = PicklistDefault // types on forward ref .js file dont work well.
+
+const options = [
+  { label: 'warning', value: 'warning' },
+  { label: 'critical', value: 'critical' },
+  { label: 'fatal', value: 'fatal' },
+]
+
+interface Props {
+  name: string
+  label: string
+  value: string
+  selectFirst: boolean
+  onChange: (value: string) => void
+}
+
+// We need to use `forwardRef` as a workaround of an issue with material-ui Tooltip https://github.com/gregnb/mui-datatables/issues/595
+const SeverityPicklist: React.ComponentType<Props> = forwardRef<HTMLElement, Props>(
+  // ( name = 'severity', label = 'Severity', onChange, selectFirst = true, ...rest }, ref) => {
+  (props, ref) => {
+    const {
+      onChange,
+      value,
+      name = 'severity',
+      label = 'Severity',
+      selectFirst = false,
+    } = props
+
+    // Select the first item as soon as data is loaded
+    useEffect(() => {
+      if (selectFirst) {
+        onChange(propOr(allKey, 'value', head(options)))
+      }
+    }, [])
+
+    return (
+      <Picklist
+        name={name}
+        label={label}
+        value={value}
+        ref={ref}
+        onChange={onChange}
+        options={options}
+      />
+    )
+  },
+)
+
+export default SeverityPicklist
