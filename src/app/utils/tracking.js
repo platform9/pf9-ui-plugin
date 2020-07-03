461ce70cfc44ecb98c0d77d5ace80436c20883f1
Kevin Lee <kevinylee1991@gmail.com>
2020-07-02 17:35:06 -0700

    PR comment fixes

--- src/app/utils/tracking.js
+++ src/app/utils/tracking.js
@@ -17,34 +17,33 @@ export const trackEvent = (name, body = {}) => {
 
 export const injectDrift = () => {
   const existingScript = document.getElementById('driftCode')
-  if (!existingScript) {
-    const script = document.createElement('script')
-    script.id = 'driftCode'
-    script.textContent = `
-      "use strict";
-      !function() {
-      var t = window.driftt = window.drift = window.driftt || [];
-      if (!t.init) {
-      if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-      t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
-      t.factory = function(e) {
-      return function() {
-      var n = Array.prototype.slice.call(arguments);
-      return n.unshift(e), t.push , t;
-      };
-      }, t.methods.forEach(function(e) {
-      t[e] = t.factory(e);
-      }), t.load = function(t) {
-      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-      var i = document.getElementsByTagName("script")[0];
-      i.parentNode.insertBefore(o, i);
-      };
-      }
-      }();
-      drift.SNIPPET_VERSION = '0.3.1';
-      drift.load('ubawwtd8c4gd');
-    `
-    document.body.appendChild(script)
-  }
+  if (existingScript) return
+  const script = document.createElement('script')
+  script.id = 'driftCode'
+  script.textContent = `
+    "use strict";
+    !function() {
+    var t = window.driftt = window.drift = window.driftt || [];
+    if (!t.init) {
+    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+    t.factory = function(e) {
+    return function() {
+    var n = Array.prototype.slice.call(arguments);
+    return n.unshift(e), t.push , t;
+    };
+    }, t.methods.forEach(function(e) {
+    t[e] = t.factory(e);
+    }), t.load = function(t) {
+    var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+    var i = document.getElementsByTagName("script")[0];
+    i.parentNode.insertBefore(o, i);
+    };
+    }
+    }();
+    drift.SNIPPET_VERSION = '0.3.1';
+    drift.load('ubawwtd8c4gd');
+  `
+  document.body.appendChild(script)
 }
