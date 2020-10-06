2f2d4e6e2c60709a79e1f7edb8a8964868220961
John Ruddell <johnruddell@gmail.com>
2020-10-05 23:26:29 -0600

    only blacklist the loading states

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -4,10 +4,18 @@ import cacheReducers, { cacheStoreKey } from 'core/caching/cacheReducers'
 import notificationReducers, { notificationStoreKey } from 'core/notifications/notificationReducers'
 import preferencesReducers, { preferencesStoreKey } from 'core/session/preferencesReducers'
 import clientReducers, { clientStoreKey } from 'core/client/clientReducers'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistReducer } from 'redux-persist'
+
+const persistCacheConfig = {
+  key: cacheStoreKey,
+  storage,
+  blacklist: ['loadingData'],
+}
 
 const rootReducer = combineReducers({
   [sessionStoreKey]: sessionReducers,
-  [cacheStoreKey]: cacheReducers,
+  [cacheStoreKey]: persistReducer(persistCacheConfig, cacheReducers),
   [notificationStoreKey]: notificationReducers,
   [preferencesStoreKey]: preferencesReducers,
   [clientStoreKey]: clientReducers,
