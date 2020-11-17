249cce4733721cc881388602470b53f1df3dd1c1
Kimberly Nguy <kimnguyy@gmail.com>
2020-11-17 12:50:27 -0800

    Removed console.log

--- src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/ssh-key-picklist.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/ssh-key-picklist.tsx
@@ -5,10 +5,6 @@ const additionalInfo =
   'Select an AWS SSH key to be associated with the nodes. This key can be used to access the nodes for debugging or other purposes.'
 
 export default ({ dropdownComponent, values, info = additionalInfo }) => {
-  console.log(
-    'disabled',
-    !(values.cloudProviderId && (values.region || values.cloudProviderRegionId)),
-  )
   return (
     <PicklistField
       DropdownComponent={dropdownComponent}
