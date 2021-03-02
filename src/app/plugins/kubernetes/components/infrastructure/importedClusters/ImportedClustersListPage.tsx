38d5c3be5175941421588d2aff16b42a38b74f91
Kevin Lee <kevinylee1991@gmail.com>
2021-03-02 09:55:11 -0800

    save progress

--- src/app/plugins/kubernetes/components/infrastructure/importedClusters/ImportedClustersListPage.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/importedClusters/ImportedClustersListPage.tsx
@@ -1,20 +1,26 @@
+import React from 'react'
+import { DateAndTime } from 'core/components/listTable/cells/DateCell'
 import createCRUDComponents from 'core/helpers/createCRUDComponents'
 // import useDataLoader from 'core/hooks/useDataLoader'
 // import React, { useEffect } from 'react'
 import { ActionDataKeys } from '../../../DataKeys'
-// import { getImportedClusters, importedClusterActions } from './actions'
+// need to include the following lines somewhere or file never loads...
+import { getImportedClusters, importedClusterActions } from './actions'
+console.log(getImportedClusters, importedClusterActions)
+
+const renderExternal = (_, { external }) => (external ? <div>External</div> : null)
 
 const columns = [
   { id: 'uuid', label: 'UUID', display: false },
   { id: 'name', label: 'Name' },
-  { id: 'external', label: 'Type' },
+  { id: 'external', label: 'Type', render: renderExternal },
   { id: 'status', label: 'Status' },
   { id: 'region', label: 'Region' },
   { id: 'kubeVersion', label: 'Kubernetes Version' },
   // { id: '', label: 'Autoscaling'},
   // { id: '', label: 'Node Groups'},
   // { id: '', label: 'Container CIDR'},
-  { id: 'creationTimestamp', label: 'Created' },
+  { id: 'creationTimestamp', label: 'Created', render: (value) => <DateAndTime value={value} /> },
 ]
 
 export const options = {
--- src/app/plugins/kubernetes/components/infrastructure/importedClusters/selectors.ts
+++ src/app/plugins/kubernetes/components/infrastructure/importedClusters/selectors.ts
@@ -6,14 +6,19 @@ export const importedClustersSelector = createSelector(
   [getDataSelector<DataKeys.ImportedClusters>(DataKeys.ImportedClusters)],
   (importedClusters) => {
     return importedClusters.map((cluster) => {
+      console.log(cluster)
       return {
         ...cluster,
-        name: cluster.metadata?.labels?.id,
+        name: cluster.spec?.displayName,
         external: cluster.metadata?.labels?.external,
         status: cluster.status?.phase,
         region: cluster.metadata?.labels?.region,
         kubeVersion: cluster.spec?.kubeVersion,
-        creationTimestamp: cluster.metadata?.creationTimestamp
+        creationTimestamp: cluster.metadata?.creationTimestamp,
+        containerCidr: cluster.spec?.eks?.network?.containerCidr,
+        servicesCidr: cluster.spec?.eks?.network?.servicesCidr,
+        nodeGroups: cluster.spec?.eks?.network?.nodeGroups,
+      }
     })
   },
 )
