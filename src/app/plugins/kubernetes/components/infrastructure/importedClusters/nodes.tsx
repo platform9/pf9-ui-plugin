c62df4911f7d7b4d2cd29a34340ff20ac631dc81
John Ruddell <johnruddell@gmail.com>
2021-03-12 01:40:05 -0700

    fixed a missing merge update

--- src/app/plugins/kubernetes/components/infrastructure/importedClusters/nodes.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/importedClusters/nodes.tsx
@@ -1,8 +1,17 @@
 import React, { useMemo } from 'react'
-import createListTableComponent from 'core/helpers/createListTableComponent'
-import { flatten, map, pipe } from 'ramda'
+import { listTablePrefs, allKey } from 'app/constants'
+import { flatten, map, pick, pipe, prop, uniqBy } from 'ramda'
 import { ImportedClusterSelector, Instance, Nodegroup } from './model'
 import ExternalLink from 'core/components/ExternalLink'
+import PicklistComponent from 'core/components/Picklist'
+import { importedClusterActions } from './actions'
+import { IUseDataLoader } from '../nodes/model'
+import useDataLoader from 'core/hooks/useDataLoader'
+import { projectAs } from 'utils/fp'
+import useReactRouter from 'use-react-router'
+import createCRUDComponents from 'core/helpers/createCRUDComponents'
+import { createUsePrefParamsHook } from 'core/hooks/useParams'
+const Picklist: any = PicklistComponent
 
 const columns = [
   { id: 'groupName', label: 'Node Group' },
@@ -19,36 +28,74 @@ const columns = [
   },
 ]
 
-interface Props {
-  cluster: ImportedClusterSelector
-  loading: boolean
-  reload: any
+const defaultParams = {
+  groupName: allKey,
 }
-const Nodes = ({ cluster, loading, reload }: Props) => {
-  const { nodeGroups } = cluster
-  const nodes = pipe<Nodegroup[], Instance[][], Instance[]>(
-    map((nodeGroup) =>
-      nodeGroup?.instances?.map((instance) => ({ ...instance, groupName: nodeGroup?.name })),
-    ),
-    flatten,
-  )(nodeGroups)
+const usePrefParams = createUsePrefParamsHook('DeployedApps', listTablePrefs)
 
-  const NodesTable = useMemo(
-    () =>
-      createListTableComponent({
-        title: 'Nodes',
-        name: 'nodes',
-        columns,
-        onReload: reload,
-        emptyText: 'Nothing yet, waiting for nodes...',
-        uniqueIdentifier: 'instanceId',
-        showCheckboxes: true,
-        multiSelection: false,
-      }),
-    [reload],
-  )
-
-  return <NodesTable data={nodes} loading={loading} />
+interface FlattenedInstances extends Instance {
+  groupName: string
 }
 
-export default Nodes
+const ListPage = ({ ListContainer }) => {
+  return () => {
+    const { params, getParamsUpdater } = usePrefParams(defaultParams)
+    const { match } = useReactRouter()
+    const [clusters, loading, reload]: IUseDataLoader<ImportedClusterSelector> = useDataLoader(
+      importedClusterActions.list,
+    ) as any
+    const cluster = clusters.find((x) => x.uuid === match.params.id)
+
+    const { nodeGroups } = cluster
+    const nodes = pipe<Nodegroup[], FlattenedInstances[][], FlattenedInstances[]>(
+      map((nodeGroup) =>
+        nodeGroup?.instances?.map((instance) => ({
+          ...instance,
+          groupName: nodeGroup?.name,
+        })),
+      ),
+      flatten,
+    )(nodeGroups)
+
+    const options = useMemo(
+      () => projectAs({ label: 'groupName', value: 'groupName' }, uniqBy(prop('groupName'), nodes)),
+      [nodeGroups],
+    )
+    const filteredNodes = nodes.filter((node) => {
+      if (params.groupName === allKey) {
+        return true
+      } else {
+        return params.groupName === node.groupName
+      }
+    })
+    return (
+      <ListContainer
+        loading={loading}
+        reload={reload}
+        data={filteredNodes}
+        getParamsUpdater={getParamsUpdater}
+        filters={
+          <>
+            <Picklist
+              onChange={getParamsUpdater('groupName')}
+              value={params.groupName}
+              options={options}
+            />
+          </>
+        }
+        {...pick(listTablePrefs, params)}
+      />
+    )
+  }
+}
+
+export const options = {
+  columns,
+  uniqueIdentifier: 'instanceId',
+  multiSelection: false,
+  ListPage,
+}
+
+const components = createCRUDComponents(options)
+
+export default components.ListPage
