7d85fd4e61b73ae0929f46ecb48fd8a3e88bcd68
Kimberly Nguy <kimnguyy@gmail.com>
2021-03-29 11:49:21 -0700

    Added data models for Helm, added some selectors for app catalog, and modif… (#1008)
    * Added types for Helm, added some selectors for app catalog, and modified deployedAppActions
    
    * Removed comments

--- src/app/plugins/kubernetes/components/apps/selectors.ts
+++ src/app/plugins/kubernetes/components/apps/selectors.ts
@@ -1,5 +1,5 @@
 import { createSelector } from 'reselect'
-import { map, mergeLeft, pipe, propEq } from 'ramda'
+import { mergeLeft, pipe, propEq } from 'ramda'
 import DataKeys from 'k8s/DataKeys'
 import getDataSelector from 'core/utils/getDataSelector'
 import createSorter from 'core/helpers/createSorter'
@@ -7,37 +7,78 @@ import { allKey } from 'app/constants'
 import { filterIf } from 'utils/fp'
 import { clustersSelector } from '../infrastructure/clusters/selectors'
 import { importedClustersSelector } from '../infrastructure/importedClusters/selectors'
+import { IDeployedAppsSelector } from './models'
+
+export const appsSelector = createSelector(
+  [getDataSelector<DataKeys.Apps>(DataKeys.Apps)],
+  (rawApps) => {
+    return rawApps.map((app) => {
+      return {
+        ...app,
+        ...app.Chart,
+      }
+    })
+  },
+)
+
+export const makeAppsSelector = (
+  defaultParams = {
+    orderBy: 'name',
+    orderDirection: 'asc',
+  },
+) => {
+  return createSelector(
+    [appsSelector, (_, params) => mergeLeft(params, defaultParams)],
+    (clusters, params) => {
+      const { orderBy, orderDirection } = params
+      return pipe(createSorter({ orderBy, orderDirection }))(clusters)
+    },
+  )
+}
+
+export const deployedAppsSelector = createSelector(
+  [getDataSelector<DataKeys.DeployedApps>(DataKeys.DeployedApps, ['clusterId']), appsSelector],
+  (rawDeployedApps, apps) => {
+    return rawDeployedApps.map((deployedApp) => {
+      const app = apps.find((app) => app.name === deployedApp.chart)
+      return {
+        ...deployedApp,
+        repository: app?.repository,
+        icon: app?.icon,
+        home: app?.home,
+      }
+    })
+  },
+)
 
 export const makeDeployedAppsSelector = (
-  defaultParams = { orderBy: 'name', orderDirection: 'asc' },
+  defaultParams = {
+    orderBy: 'name',
+    orderDirection: 'asc',
+  },
 ) => {
   return createSelector(
     [
-      getDataSelector<DataKeys.DeployedApps>(DataKeys.DeployedApps, ['clusterId']),
-      getDataSelector<DataKeys.Apps>(DataKeys.Apps),
+      deployedAppsSelector,
       clustersSelector,
       importedClustersSelector,
       (_, params) => mergeLeft(params, defaultParams),
     ],
-    (deployedApps, apps, clusters, importedClusters, params) => {
+    (deployedApps, clusters, importedClusters, params) => {
       const { clusterId, namespace, orderBy, orderDirection } = params
       const allClusters = [...clusters, ...importedClusters]
       if (!clusterId || !allClusters.find(propEq('uuid', clusterId))) {
         // If no cluster if found, this item is invalid because the cluster has been deleted
         return []
       }
-      return pipe<any, any, any, any, any>(
+      return pipe<
+        IDeployedAppsSelector[],
+        IDeployedAppsSelector[],
+        IDeployedAppsSelector[],
+        IDeployedAppsSelector[]
+      >(
         filterIf(clusterId && clusterId !== allKey, propEq('clusterId', clusterId)),
         filterIf(namespace && namespace !== allKey, propEq('namespace', namespace)),
-        map((deployedApp: any) => {
-          const app = apps.find((app) => app.name === deployedApp.chart)
-          return {
-            ...deployedApp,
-            repository: app?.repository,
-            icon: app?.icon,
-            home: app?.home,
-          }
-        }),
         createSorter({ orderBy, orderDirection }),
       )(deployedApps)
     },
