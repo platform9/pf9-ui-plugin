f0ade48d849ddfe5e96e45d90e7e6572fce726ae
kevin <kevinylee1991@gmail.com>
2021-04-01 18:09:31 -0700

    added todo comment

--- src/app/plugins/kubernetes/components/infrastructure/clusters/actions.js
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/actions.js
@@ -184,6 +184,7 @@ export const clusterActions = createCRUDActions(ActionDataKeys.Clusters, {
 // and extracts the clusterId from the first cluster
 // It also adds a "clusters" param that contains all the clusters, just for convenience
 export const parseClusterParams = async (params) => {
+  // Maybe todo: change these to use the params selector instead to enable filtering?
   const clusters = await clusterActions.list(params)
   const importedClusters = await importedClusterActions.list(params)
   const collectiveClusters = [...clusters, ...importedClusters]
