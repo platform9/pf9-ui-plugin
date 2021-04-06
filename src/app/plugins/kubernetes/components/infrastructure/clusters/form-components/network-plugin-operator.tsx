03a82a80084b87f84eb7e05a27374aa8ee589da0
Kimberly Nguy <kimnguyy@gmail.com>
2021-04-06 11:26:48 -0700

    Auto-select and gray out network backend and network operator fields when kubevirt is selected during BareOs cluster creation

--- src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/network-plugin-operator.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/network-plugin-operator.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CheckboxField from 'core/components/validatedForm/CheckboxField'
 
-const NetworkPluginOperator = () => (
+const NetworkPluginOperator = ({ wizardContext, setWizardContext }) => (
   <CheckboxField
     id="deployLuigiOperator"
     label="Network Plugin Operator"
     info="The network plugin operator will deploy Platform9 CRDs to enable multiple CNIs and features such as SR-IOV"
+    onChange={(value) => setWizardContext({ deployLuigiOperator: value })}
+    value={wizardContext.deployKubevirt ? true : wizardContext.deployLuigiOperator}
+    disabled={wizardContext.deployKubevirt}
   />
 )
 
