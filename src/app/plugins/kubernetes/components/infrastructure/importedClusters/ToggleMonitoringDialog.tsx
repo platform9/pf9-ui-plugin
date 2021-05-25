635927d3ffada3cadc16156f8b28423d68f1857e
Kimberly Nguy <kimnguyy@gmail.com>
2021-05-25 11:59:19 -0700

    UX-974/Track untracked actions and leave Bugsnag breadcrumbs for each path change and each action (#1065)
    * Added Bugsnag breadcrumbs for all actions and tracked untracked actions for Segment
    
    * Removed duplicate tracking
    
    * Changed breadcrumb type and modified du version path
    
    * Cleaned up the code. Fixed some mistakes. Added info to Bugsnag App tab
    
    * Only enable Bugsnag for prod
    
    * Added Bugsnag breadcrumbs in the trackPage function
    
    * Added return statement if route not found in trackPage

--- src/app/plugins/kubernetes/components/infrastructure/importedClusters/ToggleMonitoringDialog.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/importedClusters/ToggleMonitoringDialog.tsx
@@ -1,3 +1,4 @@
+import Bugsnag from '@bugsnag/js'
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core'
 import ApiClient from 'api-client/ApiClient'
 import Alert from 'core/components/Alert'
@@ -23,6 +24,10 @@ const ToggleMonitoringDialog = ({ rows: [cluster], onClose }) => {
   const enabled = importedHasPrometheusTag(cluster)
 
   const toggleMonitoring = useCallback(async () => {
+    Bugsnag.leaveBreadcrumb(
+      `Attempting to toggle monitoring ${enabled ? 'off' : 'on'} for imported cluster`,
+      { clusterId: cluster.uuid },
+    )
     try {
       const pkgs: any = await appbert.getPackages()
       const monPkg = pkgs.find((pkg) => pkg.name === 'pf9-mon')
