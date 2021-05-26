849df5099449e77bf43af07db87bdac6370593a0
Himanshu G <himanshug@Himanshus-MacBook-Pro.local>
2021-05-26 15:09:51 +0530

    add-label-props

--- src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/cluster-templates.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/cluster-templates.tsx
@@ -2,7 +2,7 @@ import PicklistField from 'core/components/validatedForm/PicklistField'
 import React from 'react'
 
 const ClusterTemplatesField = ({ options, onChange }) => (
-  <PicklistField id="template" label="Cluster Template" options={options} onChange={onChange} />
+  <PicklistField id="template" label="Standard_A4_v2" options={options} onChange={onChange} />
 )
 
 export default ClusterTemplatesField
