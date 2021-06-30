3c4e8836be9bf37ef3fc36045749ee17bc2c3730
rajshekhar budharam <rajshekharbudharam2015@gmail.com>
2021-06-30 12:55:49 +0530

    UX-980 updated network backend field default selection to calico option

--- src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/kubevirt-plugin-operator.tsx
+++ src/app/plugins/kubernetes/components/infrastructure/clusters/form-components/kubevirt-plugin-operator.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CheckboxField from 'core/components/validatedForm/CheckboxField'
+import { handleNetworkBackendChange } from './network-backend'
 
 const KubevirtPluginOperator = ({ wizardContext, setWizardContext }) => (
   <CheckboxField
@@ -8,7 +9,15 @@ const KubevirtPluginOperator = ({ wizardContext, setWizardContext }) => (
     info="KubeVirt enables Kubernetes to run Virtual Machines within Pods. This feature is not recommended for production workloads. "
     infoPlacement="right-end"
     value={wizardContext.deployKubevirt}
-    onChange={(value) => setWizardContext({ deployKubevirt: value })}
+    onChange={(value) => {
+      setWizardContext({
+        deployKubevirt: value,
+      })
+      if (value)
+        setWizardContext(
+          handleNetworkBackendChange('calico', wizardContext.networkStack, wizardContext),
+        )
+    }}
   />
 )
 
