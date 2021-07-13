a6dfae51be8185fa5ff48e5a549d38c7538d59c1
kingshuk.nandy <kingshuk.nandy@afourtech.com>
2021-07-14 00:25:28 +0530

    no message

--- cypress/integration/TC-GENERAL-1_login.spec.ts
+++ cypress/integration/TC-GENERAL-1_login.spec.ts
@@ -1,7 +1,9 @@
+import { loadConfig } from "cypress/support/commands"
 
 describe('User should be able to login', () => {
 
-  it('should be able to login',()=>{
+  it('should be able to login', () => {
+      loadConfig()
       cy.login()
   })
 })
